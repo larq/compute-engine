ce71aee57544ab9bf8f9f9a8e02d7ca09edd8190
Arash Bakhtiari <a.bakhtiari@icloud.com>
2019-09-11 18:49:35 +0100

    Add gtest as c++ testsuite

---
+++ larq_compute_engine/cc/tests/bgemm_tests.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+namespace compute_engine {
+namespace testing {
+
+TEST(BGemmTests, DummyTest) { EXPECT_EQ(18.0, 18.0); }
+
+}  // end namespace testing
+}  // end namespace compute_engine
