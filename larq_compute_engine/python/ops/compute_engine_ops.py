ef3d02ab989491ad67001ecf73eb7c82bf8c42f3
Arash Bakhtiari <arashb@users.noreply.github.com>
2019-10-04 18:55:48 +0100

    fix the bazel python tests (#47)

--- larq_compute_engine/python/ops/compute_engine_ops.py
+++ larq_compute_engine/python/ops/compute_engine_ops.py
@@ -1,20 +1,11 @@
 """Use larq compute engine ops in python."""
 
-import tensorflow as tf
-import os
+from tensorflow.python.framework import load_library
+from tensorflow.python.platform import resource_loader
 
+_ops_lib = load_library.load_op_library(
+    resource_loader.get_path_to_datafile("_larq_compute_engine_ops.so")
+)
 
-def get_path_to_datafile(path):
-    """Get the path to the specified file in the data dependencies.
-    Args:
-    path: a string resource path relative to the current file
-    Returns:
-    The path to the specified data file
-    """
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(root_dir, path)
-
-
-_ops_lib = tf.load_op_library(get_path_to_datafile("_larq_compute_engine_ops.so"))
 bgemm = _ops_lib.bgemm
 bsign = _ops_lib.bsign
