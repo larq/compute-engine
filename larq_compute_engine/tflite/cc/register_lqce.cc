e7a7a6e03ff7786be3712840e73c34cf14369296
Tom Bannink <tombannink@gmail.com>
2019-10-23 17:16:36 +0200

    Rename TF lite ops to new convention

--- larq_compute_engine/tflite/cc/register_lqce.cc
+++ larq_compute_engine/tflite/cc/register_lqce.cc
@@ -405,10 +405,10 @@ BuiltinOpResolver::BuiltinOpResolver() {
   AddCustom("TFLite_Detection_PostProcess",
             tflite::ops::custom::Register_DETECTION_POSTPROCESS());
 
-  AddCustom("Lqce>Bsign", compute_engine::tflite::Register_BSIGN());
-  AddCustom("Lqce>Bconv2d8", compute_engine::tflite::Register_BCONV_2D8());
-  AddCustom("Lqce>Bconv2d32", compute_engine::tflite::Register_BCONV_2D32());
-  AddCustom("Lqce>Bconv2d64", compute_engine::tflite::Register_BCONV_2D64());
+  AddCustom("LqceBsign", compute_engine::tflite::Register_BSIGN());
+  AddCustom("LqceBconv2d8", compute_engine::tflite::Register_BCONV_2D8());
+  AddCustom("LqceBconv2d32", compute_engine::tflite::Register_BCONV_2D32());
+  AddCustom("LqceBconv2d64", compute_engine::tflite::Register_BCONV_2D64());
 }
 
 }  // namespace builtin
