5868348d6f182a24276d2ddc1ee9c2161763737e
Tom Bannink <tombannink@gmail.com>
2019-10-31 11:30:21 +0100

    Simplify tflite build

--- larq_compute_engine/tflite/build/build_lqce.sh
+++ larq_compute_engine/tflite/build/build_lqce.sh
@@ -15,12 +15,31 @@ elif [ "$UNAME" = "Darwin" ] ; then
 fi
 HOST_ARCH="$(if uname -m | grep -q i[345678]86; then echo x86_32; else uname -m; fi)"
 
-if [ -f "${TF_DIR}/tensorflow/lite/tools/make/gen/${HOST_OS}_${HOST_ARCH}/lib/libtensorflow-lite.a" ]; then
-    make -j 8 BUILD_WITH_NNAPI=false -C "${ROOT_DIR}" -f larq_compute_engine/tflite/build/Makefile
-    # Also re-make the tf-lite examples so that they use the modified library
+# Use --fullbuild to also download dependencies and build the package
+# It will not download dependencies if the downloads directory already exists
+# It will not rebuild the library if all files are already up to date
+# It will, however, always rebuild the pip package
+if [ "$1" == "--fullbuild" ]; then
+    # Check if dependencies need to be downloaded
+    if [ ! -d "${TF_DIR}/tensorflow/lite/tools/make/downloads" ]; then
+        ext/tensorflow/tensorflow/lite/tools/make/download_dependencies.sh
+    fi
+    # Build tflite (will automatically skip when up-to-date
     ${TF_DIR}/tensorflow/lite/tools/make/build_lib.sh
+    # Build our kernels
+    make -j 8 BUILD_WITH_NNAPI=false -C "${ROOT_DIR}" -f larq_compute_engine/tflite/build/Makefile
+    # Rebuild tflite binaries such as benchmarking libs to include our ops
+    ${TF_DIR}/tensorflow/lite/tools/make/build_lib.sh
+    # Build the pip package
+    ext/tensorflow/tensorflow/lite/tools/pip_package/build_pip_package.sh
 else
-    echo "${HOST_OS}_${HOST_ARCH} target not found. Please build tensorflow lite first using build_lib.sh"
+    if [ -f "${TF_DIR}/tensorflow/lite/tools/make/gen/${HOST_OS}_${HOST_ARCH}/lib/libtensorflow-lite.a" ]; then
+        make -j 8 BUILD_WITH_NNAPI=false -C "${ROOT_DIR}" -f larq_compute_engine/tflite/build/Makefile
+        # Also re-make the tf-lite examples so that they use the modified library
+        ${TF_DIR}/tensorflow/lite/tools/make/build_lib.sh
+    else
+        echo "${HOST_OS}_${HOST_ARCH} target not found. Please build tensorflow lite first using build_lib.sh"
+    fi
 fi
 
 # Check if we need to cross-compile to raspberry pi
