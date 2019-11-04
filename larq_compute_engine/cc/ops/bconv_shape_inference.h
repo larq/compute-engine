91f37d2fd387ebf73f9013843487b07d0f6552de
Arash Bakhtiari <a.bakhtiari@icloud.com>
2019-11-04 13:24:15 +0000

    fixing header guard

--- larq_compute_engine/cc/ops/bconv_shape_inference.h
+++ larq_compute_engine/cc/ops/bconv_shape_inference.h
@@ -1,6 +1,10 @@
-#include "tensorflow/core/framework/common_shape_fns.h"
+#ifndef COMPUTE_ENGINE_OPS_BCONV2D_SHAPE_INF_H_
+#define COMPUTE_ENGINE_OPS_BCONV2D_SHAPE_INF_H_
+
 #include "tensorflow/core/framework/shape_inference.h"
 
 using namespace tensorflow;
 
 Status BConv2DShape(shape_inference::InferenceContext* c, int bitwidth);
+
+#endif  // COMPUTE_ENGINE_OPS_BCONV2D_SHAPE_INF_H_
