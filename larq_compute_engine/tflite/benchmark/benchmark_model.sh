e352d15f13ad24a86a95dfb29919659080606e3e
Tom Bannink <tombannink@gmail.com>
2019-11-08 15:59:58 +0100

    Output the binary name and only search one folder

--- larq_compute_engine/tflite/benchmark/benchmark_model.sh
+++ larq_compute_engine/tflite/benchmark/benchmark_model.sh
@@ -44,19 +44,13 @@ elif [ "$UNAME" = "Darwin" ] ; then
 fi
 HOST_ARCH="$(if uname -m | grep -q i[345678]86; then echo x86_32; else uname -m; fi)"
 
-if [[ $HOST_ARCH == armv7* ]]; then
+if [ "$HOST_OS" = "linux" && "$HOST_ARCH" == "armv7l" ]; then
     # Probably this is a 32-bit Raspberry Pi
-    # In this case, also check the rpi_armv7l folder
-    BENCHMARK_MODEL="${TF_DIR}/tensorflow/lite/tools/make/gen/rpi_armv7l/bin/benchmark_model"
-    if [ ! -f "${BENCHMARK_MODEL}" ]; then
-        echo "rpi_armv7l benchmark binary not found. Trying other binary as well."
-        unset BENCHMARK_MODEL
-    fi
+    # In this case check the rpi_armv7l folder instead
+    HOST_OS="rpi"
 fi
 
-# The := will not change BENCHMARK_MODEL when it was already set
-# So if the rpi_armv7l binary was found then that one is used
-BENCHMARK_MODEL:="${TF_DIR}/tensorflow/lite/tools/make/gen/${HOST_OS}_${HOST_ARCH}/bin/benchmark_model"
+BENCHMARK_MODEL="${TF_DIR}/tensorflow/lite/tools/make/gen/${HOST_OS}_${HOST_ARCH}/bin/benchmark_model"
 if [ ! -f "${BENCHMARK_MODEL}" ]; then
     echo "${HOST_OS}_${HOST_ARCH} benchmark binary not found. Please build the larq compute engine (tf lite part) first."
     exit 1
@@ -68,6 +62,8 @@ OUTPUTFILE="benchmarking_results_${current_time}.txt"
 if [ -f "$OUTPUTFILE" ]; then
     rm ${OUTPUTFILE}
 fi
+
+echo -e "Using benchmark binary: ${BENCHMARK_MODEL}\n\n" | tee --append ${OUTPUTFILE}
 
 for MODELFILE in ${ALLMODELS}
 do
