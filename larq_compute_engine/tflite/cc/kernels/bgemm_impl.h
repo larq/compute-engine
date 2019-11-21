66902f10e4ed5f6f9d085f861ba62b14dfd68879
Arash Bakhtiari <arashb@users.noreply.github.com>
2019-11-21 15:05:50 +0000

    Add RUY-based bconv2d op to TF lite (#111)
    * Use RUY gemm
    
    * Add ruy pre-packing
    
    * Add standard binary bgemm kernel to Ruy
    
    * Use RUY aligned allocator

--- larq_compute_engine/tflite/cc/kernels/bgemm_impl.h
+++ larq_compute_engine/tflite/cc/kernels/bgemm_impl.h
@@ -8,8 +8,7 @@
 #ifndef TFLITE_WITH_RUY
 #include "bgemm_impl_ref.h"
 #else
-#include "bgemm_impl_ref.h"
-// #include "bgemm_impl_ruy.h"
+#include "bgemm_impl_ruy.h"
 #endif
 
 using namespace tflite;
@@ -26,13 +25,8 @@ struct BGemmImpl : BGemmImplRef<LhsScalar, RhsScalar, AccumScalar, DstScalar,
 #else
 template <typename LhsScalar, typename RhsScalar, typename AccumScalar,
           typename DstScalar, QuantizationFlavor quantization_flavor>
-struct BGemmImpl : BGemmImplRef<LhsScalar, RhsScalar, AccumScalar, DstScalar,
-                                quantization_flavor> {};
-// TODO: currently only ref. impl. is supported
-// template <typename LhsScalar, typename RhsScalar, typename AccumScalar,
-//           typename DstScalar, QuantizationFlavor quantization_flavor>
-// struct BGemmImpl : BGemmImplUsingRuy<LhsScalar, RhsScalar, AccumScalar,
-//                                      DstScalar, quantization_flavor> {};
+struct BGemmImpl : BGemmImplUsingRuy<LhsScalar, RhsScalar, AccumScalar,
+                                     DstScalar, quantization_flavor> {};
 #endif
 
 template <typename LhsScalar, typename RhsScalar, typename AccumScalar,
--- larq_compute_engine/tflite/cc/kernels/bgemm_impl_ruy.h
+++ larq_compute_engine/tflite/cc/kernels/bgemm_impl_ruy.h
@@ -1,7 +1,9 @@
 #ifndef COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_RUY_H_
 #define COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_RUY_H_
 
-#include "tensorflow/lite/experimental/ruy/ruy.h"
+#include "bgemm_kernels_ruy.h"
+#include "tensorflow/lite/experimental/ruy/dispatch.h"
+#include "tensorflow/lite/experimental/ruy/ruy_advanced.h"
 #include "tensorflow/lite/kernels/cpu_backend_context.h"
 #include "tensorflow/lite/kernels/cpu_backend_gemm_params.h"
 
@@ -10,42 +12,6 @@ using namespace tflite::cpu_backend_gemm;
 
 namespace compute_engine {
 namespace tflite {
-
-template <typename Scalar, typename DataPointer>
-void MakeRuyMatrix(const MatrixParams<Scalar>& params, DataPointer data_ptr,
-                   ruy::Matrix<Scalar>* dst) {
-  dst->layout.rows = params.rows;
-  dst->layout.cols = params.cols;
-  if (params.order == Order::kColMajor) {
-    dst->layout.order = ruy::Order::kColMajor;
-    dst->layout.stride = params.rows;
-  } else {
-    dst->layout.order = ruy::Order::kRowMajor;
-    dst->layout.stride = params.cols;
-  }
-  // Note that ruy::Matrix::data is a ConstCheckingPtr, not a plain pointer.
-  // It does care whether we assign to it a Scalar* or a const Scalar*.
-  dst->data = data_ptr;
-  dst->zero_point = params.zero_point;
-}
-
-template <typename GemmParamsType, typename RuySpecType>
-void MakeRuySpec(const GemmParamsType& params, RuySpecType* ruy_spec) {
-  // This validation has already been performed by the Gemm API entry point,
-  // but it doesn't hurt to test specifically this again here, where it's
-  // being used.
-  ValidateGemmParams(params);
-
-  ruy_spec->multiplier_fixedpoint = params.multiplier_fixedpoint;
-  ruy_spec->multiplier_exponent = params.multiplier_exponent;
-  ruy_spec->multiplier_fixedpoint_perchannel =
-      params.multiplier_fixedpoint_perchannel;
-  ruy_spec->multiplier_exponent_perchannel =
-      params.multiplier_exponent_perchannel;
-  ruy_spec->bias = params.bias;
-  ruy_spec->clamp_min = params.clamp_min;
-  ruy_spec->clamp_max = params.clamp_max;
-}
 
 template <typename LhsScalar, typename RhsScalar, typename AccumScalar,
           typename DstScalar, QuantizationFlavor quantization_flavor>
@@ -57,20 +23,93 @@ struct BGemmImplUsingRuy {
       const GemmParams<AccumScalar, DstScalar, quantization_flavor>& params,
       CpuBackendContext* context) {
     gemmlowp::ScopedProfilingLabel label("BGemmRuy");
-    ruy::Matrix<LhsScalar> ruy_lhs;
-    ruy::Matrix<RhsScalar> ruy_rhs;
-    ruy::Matrix<DstScalar> ruy_dst;
-    MakeRuyMatrix(lhs_params, lhs_data, &ruy_lhs);
-    MakeRuyMatrix(rhs_params, rhs_data, &ruy_rhs);
-    MakeRuyMatrix(dst_params, dst_data, &ruy_dst);
 
-    ruy::BasicSpec<AccumScalar, DstScalar> ruy_spec;
-    MakeRuySpec(params, &ruy_spec);
+    static_assert(std::is_same<LhsScalar, RhsScalar>::value,
+                  "Inputs to BGEMM should have the same type.");
+    static_assert(std::is_unsigned<LhsScalar>::value &&
+                      std::is_integral<LhsScalar>::value,
+                  "Input to BGEMM should be of type unsigned integral.");
+    static_assert(std::is_signed<DstScalar>::value,
+                  "Output of BGEMM should be of a signed type.");
 
-    // TODO: need to be modified to pickup the bgemm kernel
-    // ruy::Mul<ruy::kAllPaths>(ruy_lhs, ruy_rhs, ruy_spec,
-    // context->ruy_context(),
-    //                          &ruy_dst);
+    using T = LhsScalar;
+    // default accumulator bitwidth is set to 32-bit which is enough for
+    // bitwidths we are currently using
+    using TAccum = std::int32_t;
+    using TSpec = ruy::BasicSpec<TAccum, DstScalar>;
+
+    // getting ruy context
+    auto ruy_context = context->ruy_context();
+
+    // TODO: binary path should be determined on compile time
+    constexpr auto binary_path = ruy::Path::kStandardCpp;
+
+    // avoid the reference path for production code
+    ruy::Path the_path = ruy_context->GetPathToTake<ruy::kAllPaths>();
+    RUY_CHECK_NE(the_path, ruy::Path::kReference);
+
+    // Set up the matrix layouts and spec.
+    ruy::Matrix<LhsScalar> lhs;
+    ruy::MakeSimpleLayout(lhs_params.rows, lhs_params.cols,
+                          ruy::Order::kRowMajor, &lhs.layout);
+    ruy::Matrix<RhsScalar> rhs;
+    ruy::MakeSimpleLayout(rhs_params.rows, rhs_params.cols,
+                          ruy::Order::kColMajor, &rhs.layout);
+    ruy::Matrix<DstScalar> dst;
+    ruy::MakeSimpleLayout(dst_params.rows, dst_params.cols,
+                          ruy::Order::kColMajor, &dst.layout);
+    lhs.data = lhs_data;
+    rhs.data = rhs_data;
+    dst.data = dst_data;
+
+    // Here, we abuse the 'multiplier_exponent' which is used only for
+    // non-floating-point cases to pass the bitpadding correction value (int) to
+    // bgemm kernel
+    TSpec spec;
+    spec.multiplier_exponent = params.multiplier_exponent;
+
+    // The allocator is used to allocate memory for pre-packed matrices
+    ruy::Allocator allocator;
+    auto alloc_fn = [&allocator](std::size_t num_bytes) -> void* {
+      return allocator.AllocateBytes(num_bytes);
+    };
+
+    // pre-pack the lhs and rhs matrices
+    ruy::PrepackedMatrix prepacked_lhs;
+    ruy::PrepackedMatrix prepacked_rhs;
+    ruy::PrePackForMul<binary_path>(lhs, rhs, spec, ruy_context, &dst,
+                                    &prepacked_lhs, &prepacked_rhs, alloc_fn);
+
+    // In Ruy, TrMul is computed instead of Mul, therefore the lhs needs to be
+    // transposed. Transpose function is cheap since it does not shuffle data
+    // around and only changes the matrix layout.
+    ruy::Matrix<LhsScalar> transposed_lhs(lhs);
+    ruy::Transpose(&transposed_lhs);
+
+    // Based on the Path, kernel function pointers are set in TrMulParams
+    // constexpr ruy::Path TrMulCompiledPaths =
+    //     ruy::kAllPaths & ~ruy::Path::kReference;
+    ruy::TrMulParams trmul_params;
+    ruy::CreateTrMulParams<binary_path>(transposed_lhs, rhs, spec, ruy_context,
+                                        &dst, binary_path, &trmul_params);
+
+    // set the pre-packed params
+    trmul_params.packed[ruy::Side::kLhs].data = prepacked_lhs.data;
+    trmul_params.packed[ruy::Side::kLhs].sums = prepacked_lhs.sums;
+    trmul_params.is_prepacked[ruy::Side::kLhs] = true;
+
+    trmul_params.packed[ruy::Side::kRhs].data = prepacked_rhs.data;
+    trmul_params.packed[ruy::Side::kRhs].sums = prepacked_rhs.sums;
+    trmul_params.is_prepacked[ruy::Side::kRhs] = true;
+
+    // redirect the kernel function pointer to the binary kernel
+    using PackedLhsScalar = ruy::PackedType<binary_path, LhsScalar>;
+    using PackedRhsScalar = ruy::PackedType<binary_path, RhsScalar>;
+    trmul_params.run_kernel =
+        &RunBgemmKernel<binary_path, PackedLhsScalar, PackedRhsScalar,
+                        DstScalar, TSpec>;
+
+    ruy::TrMul(&trmul_params, ruy_context);
   }
 };
 
