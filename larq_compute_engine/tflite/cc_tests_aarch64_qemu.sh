52e358ccacad434dead63c962ac58f9e08ecd59c
Tom Bannink <tombannink@gmail.com>
2019-11-25 17:56:36 +0100

    Add shell script to run tests on ARM with qemu

---
+++ larq_compute_engine/tflite/cc_tests_aarch64_qemu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+export QEMU_LD_PREFIX="external/aarch64_compiler/aarch64-linux-gnu/libc"
+
+for testfile in larq_compute_engine/tflite/*_test*;
+do
+    qemu-aarch64 "$testfile"
+done
