d41298a036e02987814ec7f8e953af62d9802e8c
Tom Bannink <tombannink@gmail.com>
2019-12-10 14:25:09 +0100

    Add page-aligned benchmark

--- larq_compute_engine/cc/benchmark/aarch64/packbits_bench.cc
+++ larq_compute_engine/cc/benchmark/aarch64/packbits_bench.cc
@@ -5,12 +5,24 @@
 
 #include <benchmark/benchmark.h>
 
-#include <vector>
+#include <cstdlib>
 
 #include "larq_compute_engine/cc/core/aarch64/packbits.h"
 #include "larq_compute_engine/cc/core/packbits.h"
 
 using namespace compute_engine::core;
+
+constexpr size_t min_size = 128;
+constexpr size_t max_size = 1 << 16;  // 64K
+
+// Benchmark results seem to depend on memory alignment,
+// not only to cache-size (64 bytes) but also page size (4KB).
+// To get a fair comparison, we test all functions on the same memory.
+constexpr size_t alignment = 4 * 1024;
+float* in_array = reinterpret_cast<float*>(
+    aligned_alloc(alignment, max_size * sizeof(float)));
+uint64_t* out_array = reinterpret_cast<uint64_t*>(
+    aligned_alloc(alignment, (max_size / 64) * sizeof(uint64_t)));
 
 template <void (*packing_func)(const float*, size_t, uint64_t*),
           size_t blocksize>
@@ -20,20 +32,15 @@ static void packbits_bench(benchmark::State& state) {
 
   const size_t num_blocks = in_size / blocksize;
 
-  std::vector<float> in(in_size, -1.0f);
-  std::vector<uint64_t> out(out_size);
-
   for (auto _ : state) {
-    benchmark::DoNotOptimize(out.data());
-    packing_func(in.data(), num_blocks, out.data());
-    benchmark::ClobberMemory();  // Force out data to be written to memory
+    benchmark::DoNotOptimize(out_array);
+    packing_func(in_array, num_blocks, out_array);
+    benchmark::ClobberMemory();  // Force output data to be written to memory
   }
   // Print memory alignment to check if that affects results
-  state.counters["alignment"] = (reinterpret_cast<uint64_t>(in.data()) % 64);
+  state.counters["alignment"] = (reinterpret_cast<uint64_t>(in_array) % 4096);
 }
 
-constexpr size_t min_size = 128;
-constexpr size_t max_size = 1 << 18;  // 256K
 BENCHMARK_TEMPLATE(packbits_bench, packbits_array<float, uint64_t>, 1)
     ->Range(min_size, max_size);
 BENCHMARK_TEMPLATE(packbits_bench, packbits_aarch64_64, 64)
