b253a026d6cb921cec2bbc8fa679aa8c00623cb9
Arash Bakhtiari <a.bakhtiari@icloud.com>
2020-01-13 16:28:37 +0000

    creating LCE TF lite ops library with bazel

--- larq_compute_engine/tflite/cc_tests_aarch64_qemu.sh
+++ larq_compute_engine/tflite/cc_tests_aarch64_qemu.sh
@@ -4,7 +4,7 @@ set -e
 
 export QEMU_LD_PREFIX="external/aarch64_compiler/aarch64-linux-gnu/libc"
 
-for testfile in larq_compute_engine/tflite/*_test;
+for testfile in larq_compute_engine/tflite/tests/*_test;
 do
     qemu-aarch64 "$testfile"
 done
