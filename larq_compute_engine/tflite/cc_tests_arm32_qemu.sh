d401a0a1bee9df31f5b8a02d0d1900402ee661f2
Arash Bakhtiari <arashb@users.noreply.github.com>
2020-01-14 16:44:12 +0000

    Add a bazel target for LCE library (#135)

--- larq_compute_engine/tflite/cc_tests_arm32_qemu.sh
+++ larq_compute_engine/tflite/cc_tests_arm32_qemu.sh
@@ -4,7 +4,7 @@ set -e
 
 export QEMU_LD_PREFIX="external/arm_compiler/arm-rpi-linux-gnueabihf/sysroot"
 
-for testfile in larq_compute_engine/tflite/*_test;
+for testfile in larq_compute_engine/tflite/tests/*_test;
 do
     qemu-arm "$testfile"
 done
