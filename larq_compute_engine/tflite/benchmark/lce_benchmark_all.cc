6bbbc8c3727d1746a410c0891b7906c671e13167
Arash Bakhtiari <arashb@users.noreply.github.com>
2020-01-23 15:49:35 +0000

    Add mobilenet benchmarks (#153)

--- larq_compute_engine/tflite/benchmark/lce_benchmark_all.cc
+++ larq_compute_engine/tflite/benchmark/lce_benchmark_all.cc
@@ -35,6 +35,16 @@ static void BM_model_inference(benchmark::State& state, const char* filename) {
 
 // NOTE: the path to the benchmarking models are hardcoded here. Modify the
 // paths based on your setup.
+// clang-format off
+BENCHMARK_CAPTURE(BM_model_inference, mobilenet_v2_0.5_224,
+                  "/data/local/tmp/mobilenet_v2_0.5_224.tflite");
+BENCHMARK_CAPTURE(BM_model_inference, mobilenet_v2_0.75_224,
+                  "/data/local/tmp/mobilenet_v2_0.75_224.tflite");
+BENCHMARK_CAPTURE(BM_model_inference, mobilenet_v2_1.0_224,
+                  "/data/local/tmp/mobilenet_v2_1.0_224.tflite");
+BENCHMARK_CAPTURE(BM_model_inference, mobilenet_v2_1.0_224_quant,
+                  "/data/local/tmp/mobilenet_v2_1.0_224_quant.tflite");
+// clang-format on
 BENCHMARK_CAPTURE(BM_model_inference, birealnet_float,
                   "/data/local/tmp/birealnet_float.tflite");
 BENCHMARK_CAPTURE(BM_model_inference, birealnet_binary,
