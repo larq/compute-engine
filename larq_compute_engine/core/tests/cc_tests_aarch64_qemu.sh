ff4ddf881173df14c7a163869dd752421248d69e
Arash Bakhtiari <arashb@users.noreply.github.com>
2020-01-31 17:49:33 +0000

    refactoring: lce/tf/{bench|tests} -> lce/core/{bench|tests} (#165)

--- larq_compute_engine/core/tests/cc_tests_aarch64_qemu.sh
+++ larq_compute_engine/core/tests/cc_tests_aarch64_qemu.sh
@@ -4,7 +4,7 @@ set -e
 
 export QEMU_LD_PREFIX="external/aarch64_compiler/aarch64-linux-gnu/libc"
 
-for testfile in larq_compute_engine/tf/tests/*_tests;
+for testfile in larq_compute_engine/core/tests/*_tests;
 do
     qemu-aarch64 "$testfile"
 done
