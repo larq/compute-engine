836d312623a13638fc5a204dff9f6565861cf1b1
Lukas Geiger <lukas.geiger94@gmail.com>
2020-02-08 16:03:10 +0000

    Simplify convert binary to reduce dependence on TensorFlow
    We probably want to keep the nice error handling from MLIR, but let's make it simple first.

--- larq_compute_engine/mlir/tf_lce_translate.cc
+++ larq_compute_engine/mlir/tf_lce_translate.cc
@@ -1,28 +1,18 @@
 #include "larq_compute_engine/mlir/tf_tfl_passes.h"
-#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/ToolOutputFile.h"
-#include "mlir/IR/Function.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Module.h"
 #include "mlir/Support/FileUtilities.h"
-#include "tensorflow/compiler/mlir/init_mlir.h"
+#include "tensorflow/compiler/mlir/lite/flatbuffer_translate.h"
 #include "tensorflow/compiler/mlir/lite/tf_tfl_translate_cl.h"
-#include "tensorflow/compiler/mlir/lite/tf_to_tfl_flatbuffer.h"
+#include "tensorflow/compiler/mlir/tensorflow/translate/tf_mlir_translate.h"
 #include "tensorflow/compiler/mlir/tensorflow/translate/tf_mlir_translate_cl.h"
-#include "tensorflow/stream_executor/lib/statusor.h"
-
-using mlir::FuncOp;
-using mlir::MLIRContext;
-using mlir::ModuleOp;
-using stream_executor::port::StatusOr;
 
 enum TranslationStatus { kTrSuccess, kTrFailure };
 
 int main(int argc, char** argv) {
-  // TODO(jpienaar): Revise the command line option parsing here.
-  tensorflow::InitMlir y(&argc, &argv);
-
-  // TODO(antiagainst): We are pulling in multiple transformations as follows.
   // Each transformation has its own set of command-line options; options of one
   // transformation can essentially be aliases to another. For example, the
   // -tfl-annotate-inputs has -tfl-input-arrays, -tfl-input-data-types, and
@@ -34,34 +24,36 @@ int main(int argc, char** argv) {
   llvm::cl::ParseCommandLineOptions(
       argc, argv, "TF GraphDef to TFLite FlatBuffer converter\n");
 
-  MLIRContext context;
-  llvm::SourceMgr source_mgr;
-  mlir::SourceMgrDiagnosticHandler sourceMgrHandler(source_mgr, &context);
+  // Set up the input file.
+  std::string error_message;
+  auto file = mlir::openInputFile(input_file_name, &error_message);
+  if (!file) {
+    llvm::errs() << error_message << "\n";
+    return kTrFailure;
+  }
 
-  StatusOr<mlir::OwningModuleRef> module =
-      tensorflow::LoadFromGraphdefOrMlirSource(
-          input_file_name, input_mlir, use_splatted_constant, custom_opdefs,
-          debug_info_file, input_arrays, input_dtypes, input_shapes,
-          output_arrays, /*prune_unused_nodes=*/true, &source_mgr, &context,
-          /*add_pseudo_input_nodes=*/false);
-
-  // If errors occur, the library call in the above already logged the error
-  // message. So we can just return here.
-  if (!module.ok()) return kTrFailure;
+  mlir::MLIRContext context;
+  mlir::OwningModuleRef module = tensorflow::GraphdefToMlirTranslateFunction(
+      file->getBuffer(), debug_info_file, input_arrays, input_dtypes,
+      input_shapes, output_arrays, /*prune_unused_nodes=*/true,
+      /*convert_legacy_fed_inputs=*/true, /*graph_as_function=*/false,
+      /*upgrade_legacy=*/true, /*add_pseudo_input_nodes=*/false, &context);
 
   mlir::PassManager pm(&context);
-
   tensorflow::AddTFToLCETFLConversionPasses(&pm);
 
-  mlir::TFL::QuantizationSpecs quant_specs;
+  if (failed(pm.run(module.get()))) {
+    return kTrFailure;
+  }
 
   std::string result;
-  auto status = tensorflow::ConvertTFExecutorToTFLOrFlatbuffer(
-      module.ValueOrDie().get(), output_mlir, /*emit_builtin_tflite_ops=*/true,
-      /*emit_select_tf_ops=*/false, /*emit_custom_ops=*/true,
-      /*emit_quant_adaptor_ops=*/false, /*lower_tensor_list_ops=*/false,
-      quant_specs, &result, &pm, /*add_pseudo_input_nodes=*/false);
-  if (!status.ok()) return kTrFailure;
+  // Write MLIR TFLite dialect into FlatBuffer
+  if (tflite::MlirToFlatBufferTranslateFunction(
+          module.get(), &result, /*emit_builtin_tflite_ops=*/true,
+          /*emit_select_tf_ops=*/false, /*emit_custom_ops=*/true,
+          /*add_pseudo_input_nodes=*/false)) {
+    return kTrFailure;
+  }
 
   std::string error_msg;
   auto output = mlir::openOutputFile(output_file_name, &error_msg);
