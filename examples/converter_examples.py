ffa909512441507e35a82c0649020b7e34c60b5b
Tom Bannink <tom@plumerai.com>
2020-02-14 11:34:45 +0100

    Apply suggestions from code review
    Co-Authored-By: Lukas Geiger <lgeiger@users.noreply.github.com>

--- examples/converter_examples.py
+++ examples/converter_examples.py
@@ -6,9 +6,11 @@ import larq_zoo as lqz
 # Example of converting a model from Larq Zoo to TF lite
 model = lqz.BinaryResNetE18(weights="imagenet")
 converted = lce.convert_keras_model(model)
-open("binaryresnete18.tflite", "wb").write(converted)
+with open("binaryresnete18.tflite", "wb") as f:
+    f.write(converted)
 
 # Example of converting an h5 file
 model = tf.keras.models.load_model("my_model.h5")
 converted = lce.convert_keras_model(model)
-open("my_model.tflite", "wb").write(converted)
+with open("my_model.tflite", "wb") as f:
+    f.write(converted)
