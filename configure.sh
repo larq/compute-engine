cb927b3fc6286ad0b65f366456a0952560ef9e6a
koenhelwegen <koen@plumerai.com>
2020-02-17 14:22:57 +0000

    Add larq modification notices to tensorflow copyrighted files.

--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+# Modifications copyright (C) 2020 Larq Contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
