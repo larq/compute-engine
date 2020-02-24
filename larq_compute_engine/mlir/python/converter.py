7e99614fbc144364c06ac380e30d5c067b9b8637
Lukas Geiger <lgeiger@users.noreply.github.com>
2020-02-24 11:29:18 +0000

    Improve converter API docs (#237)

--- larq_compute_engine/mlir/python/converter.py
+++ larq_compute_engine/mlir/python/converter.py
@@ -37,11 +37,21 @@ def concrete_function_from_keras_model(model):
     return func.get_concrete_function()
 
 
-def convert_keras_model(model):
+def convert_keras_model(model: tf.keras.Model) -> bytes:
     """Converts a Keras model to TFLite flatbuffer.
 
-    Returns:
-      The converted data in serialized format.
+    !!! example
+        ```python
+        tflite_model = convert_keras_model(model)
+        with open("/tmp/my_model.tflite", "wb") as f:
+            f.write(tflite_model)
+        ```
+
+    # Arguments
+    model: A [`tf.keras.Model`](https://www.tensorflow.org/api_docs/python/tf/keras/Model) to convert.
+
+    # Returns
+    The converted data in serialized format.
     """
     if not tf.executing_eagerly():
         raise RuntimeError(
