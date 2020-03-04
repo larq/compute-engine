30cf0718b3098a821900e75321aa84d521ca8380
Arash Bakhtiari <arashb@users.noreply.github.com>
2020-03-04 11:22:20 +0000

    remove legacy reference impl. of LCE BConv Op (#261)

--- larq_compute_engine/core/benchmark/bgemm_bench.cc
+++ larq_compute_engine/core/benchmark/bgemm_bench.cc
@@ -3,7 +3,6 @@
 #include <vector>
 
 #include "larq_compute_engine/core/bgemm_functor.h"
-#include "larq_compute_engine/core/fused_bgemm_functor.h"
 
 using namespace compute_engine::core;
 using compute_engine::core::Layout;
@@ -50,54 +49,9 @@ static void bgemm(benchmark::State& state) {
   }
 }
 
-template <typename TIn, typename TBitpacked, typename TOut>
-static void fused_bgemm(benchmark::State& state) {
-  const int input_height = state.range(0);
-  const int input_width = state.range(0);
-  const int filter_width = state.range(1);
-  const int filter_height = state.range(1);
-  const int channels_in = state.range(2);
-  const int channels_out = state.range(3);
-
-  const int m = input_height * input_width;
-  const int n = channels_out;
-  const int k = filter_height * filter_width * channels_in;
-
-  const int a_size = m * k;
-  const int b_size = k * n;
-  const int c_size = m * n;
-
-  std::vector<TIn> a_data_float(a_size, 1.0f);
-  std::vector<TIn> b_data_float(b_size, -1.0f);
-  std::vector<TOut> c(c_size);
-
-  const int lda = k;
-  const int ldb = n;
-  const int ldc = n;
-
-  using TBGemmFunctor =
-      ReferenceBGemmFunctor<TBitpacked, Layout::RowMajor, TBitpacked,
-                            Layout::ColMajor, TOut>;
-  using TFusedBGemmFunctor =
-      FusedBGemmFunctor<TIn, Layout::RowMajor, TIn, Layout::ColMajor, TOut,
-                        TBitpacked, TBGemmFunctor>;
-
-  TFusedBGemmFunctor fused_bgemm_functor;
-  for (auto _ : state) {
-    fused_bgemm_functor(m, n, k, a_data_float.data(), lda, b_data_float.data(),
-                        ldb, c.data(), ldc);
-  }
-}
-
 BENCHMARK_TEMPLATE(bgemm, uint8_t, float)
     ->Ranges({{16, 32}, {3, 5}, {8, 128}, {4, 16}});
 BENCHMARK_TEMPLATE(bgemm, uint32_t, float)
     ->Ranges({{16, 32}, {3, 5}, {8, 128}, {4, 16}});
 BENCHMARK_TEMPLATE(bgemm, uint64_t, float)
-    ->Ranges({{16, 32}, {3, 5}, {8, 128}, {4, 16}});
-BENCHMARK_TEMPLATE(fused_bgemm, float, uint8_t, float)
-    ->Ranges({{16, 32}, {3, 5}, {8, 128}, {4, 16}});
-BENCHMARK_TEMPLATE(fused_bgemm, float, uint32_t, float)
-    ->Ranges({{16, 32}, {3, 5}, {8, 128}, {4, 16}});
-BENCHMARK_TEMPLATE(fused_bgemm, float, uint64_t, float)
     ->Ranges({{16, 32}, {3, 5}, {8, 128}, {4, 16}});
