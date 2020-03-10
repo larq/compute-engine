652b34bd2be9d9bcb9136623acd58046b56076eb
Lukas Geiger <lukas.geiger94@gmail.com>
2020-03-10 14:38:33 +0000

    Force eager mode for TF 1.x

--- larq_compute_engine/mlir/python/converter_test.py
+++ larq_compute_engine/mlir/python/converter_test.py
@@ -3,6 +3,7 @@ import unittest
 from unittest import mock
 
 import larq_zoo as lqz
+from tensorflow.python.eager import context
 
 sys.modules["larq_compute_engine.mlir._graphdef_tfl_flatbuffer"] = mock.MagicMock()
 
@@ -14,9 +15,9 @@ from larq_compute_engine.mlir._graphdef_tfl_flatbuffer import (
 
 class TestConverter(unittest.TestCase):
     def test_larq_zoo_models(self):
-        model = lqz.BinaryResNetE18(weights=None)
-
-        convert_keras_model(model)
+        with context.eager_mode():
+            model = lqz.BinaryResNetE18(weights=None)
+            convert_keras_model(model)
         mocked_converter.assert_called_once_with(
             mock.ANY, ["input_1"], ["DT_FLOAT"], [[1, 224, 224, 3]], ["Identity"],
         )
