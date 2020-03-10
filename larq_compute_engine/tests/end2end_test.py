f22dd1687ba4f793e623bf2be0ed447197bdfc7a
Tom Bannink <tom@plumerai.com>
2020-03-10 15:14:08 +0100

    Add end2end test (#269)
    * Add end2end test
    
    * Run batched inference in end2end test
    
    * Move end2end tests to separate location
    
    Co-authored-by: Lukas Geiger <lukas.geiger94@gmail.com>

---
+++ larq_compute_engine/tests/end2end_test.py
@@ -0,0 +1,86 @@
+import sys
+import pytest
+import larq as lq
+import larq_zoo as lqz
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+
+from larq_compute_engine.mlir.python.converter import convert_keras_model
+from larq_compute_engine.tests._end2end_verify import run_model
+
+
+def toy_model(**kwargs):
+    def block(padding, pad_values, activation):
+        def dummy(x):
+            shortcut = x
+            x = lq.layers.QuantConv2D(
+                filters=32,
+                kernel_size=3,
+                padding=padding,
+                # pad_values=pad_values,
+                input_quantizer="ste_sign",
+                kernel_quantizer="ste_sign",
+                use_bias=False,
+                activation=activation,
+            )(x)
+            x = tf.keras.layers.BatchNormalization(
+                gamma_initializer=tf.keras.initializers.RandomNormal(1.0),
+                beta_initializer="uniform",
+            )(x)
+            return tf.keras.layers.add([x, shortcut])
+
+        return dummy
+
+    img_input = tf.keras.layers.Input(shape=(224, 224, 3))
+    out = img_input
+    out = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same")(out)
+
+    # Test zero-padding
+    out = block("same", 0.0, "relu")(out)
+    # Test one-padding
+    out = block("same", 1.0, "relu")(out)
+    # Test no activation function
+    out = block("same", 1.0, None)(out)
+
+    out = tf.keras.layers.GlobalAvgPool2D()(out)
+    return tf.keras.Model(inputs=img_input, outputs=out)
+
+
+def preprocess(data):
+    return lqz.preprocess_input(data["image"])
+
+
+@pytest.mark.parametrize(
+    "model_cls", [toy_model, lqz.BinaryResNetE18],
+)
+def test_simple_model(model_cls):
+    model = model_cls(weights="imagenet")
+    model_lce = convert_keras_model(model)
+
+    # Test on the flowers dataset
+    dataset = (
+        tfds.load("oxford_flowers102", split="validation")
+        .map(preprocess)
+        .shuffle(256)
+        .batch(10)
+        .take(1)
+    )
+    inputs = next(tfds.as_numpy(dataset))
+
+    outputs = model(inputs).numpy()
+    for input, output in zip(inputs, outputs):
+        for actual_output in run_model(model_lce, list(input.flatten())):
+            np.testing.assert_allclose(actual_output, output, rtol=0.001, atol=0.25)
+
+    # Test on some random inputs
+    input_shape = (10, *model.input.shape[1:])
+    inputs = np.random.uniform(-1, 1, size=input_shape).astype(np.float32)
+    outputs = model(inputs).numpy()
+    for input, output in zip(inputs, outputs):
+        for actual_output in run_model(model_lce, list(input.flatten())):
+            np.testing.assert_allclose(actual_output, output, rtol=0.001, atol=0.25)
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__, "-s"]))
---
+++ larq_compute_engine/tests/end2end_verify.cc
@@ -0,0 +1,97 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#include "larq_compute_engine/tflite/kernels/lce_ops_register.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
+#include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/model.h"
+#include "tensorflow/lite/optional_debug_tools.h"
+
+// This file is based on the TF lite minimal example
+// Its used in combination with the python/end2end_test.py script
+
+using namespace tflite;
+
+bool getFlatTensor(Interpreter* interpreter, int tensor_id, float** data,
+                   int* num_elements) {
+  const auto* tensor = interpreter->tensor(tensor_id);
+
+  *data = interpreter->typed_tensor<float>(tensor_id);
+
+  if (!*data || tensor->dims == nullptr || tensor->dims->size == 0)
+    return false;
+
+  *num_elements = 1;
+  for (int i = 0; i < tensor->dims->size; ++i)
+    *num_elements *= tensor->dims->data[i];
+
+  return true;
+}
+
+#define MINIMAL_CHECK(x)                                    \
+  if (!(x)) {                                               \
+    std::cerr << "Error at line " << __LINE__ << std::endl; \
+    return result;                                          \
+  }
+
+std::vector<std::vector<float>> runModel(const pybind11::bytes& _flatbuffer,
+                                         const std::vector<float>& input) {
+  std::vector<std::vector<float>> result;
+
+  // Load model
+  const auto flatbuffer = static_cast<std::string>(_flatbuffer);
+  std::unique_ptr<tflite::FlatBufferModel> model =
+      tflite::FlatBufferModel::BuildFromBuffer(flatbuffer.data(),
+                                               flatbuffer.size());
+  MINIMAL_CHECK(model != nullptr);
+
+  // Build the interpreter
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+  compute_engine::tflite::RegisterLCECustomOps(&resolver);
+
+  InterpreterBuilder builder(*model, resolver);
+  std::unique_ptr<Interpreter> interpreter;
+  builder(&interpreter);
+  MINIMAL_CHECK(interpreter != nullptr);
+
+  // Allocate tensor buffers.
+  MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
+
+  auto input_ids = interpreter->inputs();
+  auto output_ids = interpreter->outputs();
+
+  MINIMAL_CHECK(input_ids.size() == 1);
+  MINIMAL_CHECK(output_ids.size() == 1);
+
+  float* input_tensor_data;
+  int num_input_elements;
+  MINIMAL_CHECK(getFlatTensor(interpreter.get(), input_ids[0],
+                              &input_tensor_data, &num_input_elements));
+  MINIMAL_CHECK(num_input_elements == (int)input.size());
+
+  float* output_tensor_data;
+  int num_output_elements;
+  MINIMAL_CHECK(getFlatTensor(interpreter.get(), output_ids[0],
+                              &output_tensor_data, &num_output_elements));
+
+  // Do inference twice, to check if the warmpup run and internal temporary
+  // buffers work as expected
+  for (int run = 0; run < 2; ++run) {
+    // Fill input buffers
+    for (int i = 0; i < num_input_elements; ++i) {
+      input_tensor_data[i] = input[i];
+    }
+    // Run inference
+    MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
+    // Store output buffers
+    result.push_back(std::vector<float>(
+        &output_tensor_data[0], &output_tensor_data[num_output_elements]));
+  }
+
+  return result;
+}
+
+PYBIND11_MODULE(_end2end_verify, m) { m.def("run_model", &runModel); };
