59d7836105104e76ea9111f2df209b6e2c8306af
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-03-15 22:14:46 +0000

    Implement SSE 4.2 and VNNI placeholder kernels. (#286)

--- larq_compute_engine/core/bgemm_kernels_x86.h
+++ larq_compute_engine/core/bgemm_kernels_x86.h
@@ -61,6 +61,52 @@ struct BgemmKernel<ruy::Path::kAvx512, LhsScalar, RhsScalar, DstScalar, Spec> {
   }
 };
 
+template <typename LhsScalar, typename RhsScalar, typename DstScalar,
+          typename Spec>
+struct BgemmKernel<ruy::Path::kAvxVnni, LhsScalar, RhsScalar, DstScalar, Spec> {
+  Tuning tuning = Tuning::kAuto;
+  using LhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 16>;
+  using RhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 16>;
+  explicit BgemmKernel(Tuning tuning_) : tuning(tuning_) {}
+  void Run(const ruy::PackedMatrix<LhsScalar>& lhs,
+           const ruy::PackedMatrix<RhsScalar>& rhs, const Spec& spec,
+           int start_row, int start_col, int end_row, int end_col,
+           ruy::Matrix<DstScalar>* dst) const {
+    static_assert(std::is_same<LhsScalar, RhsScalar>::value,
+                  "Inputs to binary kernel should have the same type.");
+    static_assert(
+        // std::is_unsigned<LhsScalar>::value &&
+        std::is_integral<LhsScalar>::value,
+        "Input to binary kernel should be of type unsigned integral.");
+    static_assert(std::is_signed<DstScalar>::value,
+                  "Output of binary kernel should be of a signed type.");
+    // TODO: not implemented -> fallback to standard cpp
+  }
+};
+
+template <typename LhsScalar, typename RhsScalar, typename DstScalar,
+          typename Spec>
+struct BgemmKernel<Path::kSse42, LhsScalar, RhsScalar, DstScalar, Spec> {
+  Tuning tuning = Tuning::kAuto;
+  using LhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
+  using RhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
+  explicit BgemmKernel(Tuning tuning_) : tuning(tuning_) {}
+  void Run(const ruy::PackedMatrix<LhsScalar>& lhs,
+           const ruy::PackedMatrix<RhsScalar>& rhs, const Spec& spec,
+           int start_row, int start_col, int end_row, int end_col,
+           ruy::Matrix<DstScalar>* dst) const {
+    static_assert(std::is_same<LhsScalar, RhsScalar>::value,
+                  "Inputs to binary kernel should have the same type.");
+    static_assert(
+        // std::is_unsigned<LhsScalar>::value &&
+        std::is_integral<LhsScalar>::value,
+        "Input to binary kernel should be of type unsigned integral.");
+    static_assert(std::is_signed<DstScalar>::value,
+                  "Output of binary kernel should be of a signed type.");
+    // TODO: not implemented -> fallback to standard cpp
+  }
+};
+
 #endif  // RUY_PLATFORM(X86)
 
 #endif  // COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_KERNELS_X86_H_
