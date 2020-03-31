bcb58343f1fe7e86a39357b290e98fe3010dc10a
Arash Bakhtiari <arashb@users.noreply.github.com>
2020-03-31 16:47:00 +0100

    Add reference implementation of binary convolution. (#314)
    * Add reference implementation of binary convolution.
    
    Co-authored-by: Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
    Co-authored-by: Tom Bannink <tom@plumerai.com>

--- larq_compute_engine/tflite/tests/bconv2d_test.cc
+++ larq_compute_engine/tflite/tests/bconv2d_test.cc
@@ -166,6 +166,7 @@ using namespace tflite;
 namespace compute_engine {
 namespace tflite {
 
+TfLiteRegistration* Register_BCONV_2D32_REF();
 TfLiteRegistration* Register_BCONV_2D32_OPT();
 TfLiteRegistration* Register_BCONV_2D64_OPT();
 
@@ -333,8 +334,9 @@ class BConv2DOpModel : public BaseBConv2DOpModel {
 };
 
 const auto kKernelMap = new std::map<string, register_function>({
-    {"BConv2D32", compute_engine::tflite::Register_BCONV_2D32_OPT},
-    {"BConv2D64", compute_engine::tflite::Register_BCONV_2D64_OPT},
+    {"BConv2D32REF", compute_engine::tflite::Register_BCONV_2D32_REF},
+    {"BConv2D32OPT", compute_engine::tflite::Register_BCONV_2D32_OPT},
+    {"BConv2D64OPT", compute_engine::tflite::Register_BCONV_2D64_OPT},
 });
 
 class BConv2DOpTest : public ::testing::TestWithParam<TestParamTuple> {
@@ -417,6 +419,16 @@ TEST_P(BConv2DOpTest, SimpleTest) {
   const int packed_num_elem =
       filter_count * filter_height * filter_width * packed_channels;
 
+  // the reference implementation only support one-padding
+  const auto is_reference_registration =
+      (registration == compute_engine::tflite::Register_BCONV_2D32_REF);
+
+  if ((padding == Padding_SAME && pad_values == 0) &&
+      is_reference_registration) {
+    GTEST_SKIP();
+    return;
+  }
+
   using T = float;
   std::vector<T> input_data, padded_input_data, filters_data;
   std::vector<T> post_activation_multiplier_data, post_activation_bias_data,
@@ -426,7 +438,7 @@ TEST_P(BConv2DOpTest, SimpleTest) {
   filters_data.resize(filters_num_elem);
   packed_filters_data.resize(packed_num_elem);
   bias_data.resize(filter_count, 0);
-  post_activation_multiplier_data.resize(filter_count, 0);
+  post_activation_multiplier_data.resize(filter_count, 1);
   post_activation_bias_data.resize(filter_count, 0);
 
   std::random_device rd;
