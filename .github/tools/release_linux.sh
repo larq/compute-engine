bf6194cac87257312f478c456cc8924e62348551
Lukas Geiger <lukas.geiger94@gmail.com>
2020-05-07 01:44:25 +0100

    Add Python 3.8 builds

--- .github/tools/release_linux.sh
+++ .github/tools/release_linux.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -e -x
 
-# Remove the now private ppa. This can be removed after the docker image removes the
-# pre-installed python packages from this ppa.
-rm -f /etc/apt/sources.list.d/jonathonf-ubuntu-python-3_6-xenial.list
-
 ln -sf /usr/bin/python3.5 /usr/bin/python3 # Py36 has issues with add-apt
 add-apt-repository -y ppa:deadsnakes/ppa
 
 apt-get -y -qq update
 
-apt-get -y -qq install python${PYTHON_VERSION}-dev --no-install-recommends
+apt-get -y -qq install python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-distutils --no-install-recommends
 ln -sf /usr/bin/python${PYTHON_VERSION} /usr/bin/python
 ln -sf /usr/bin/python${PYTHON_VERSION} /usr/bin/python3
 
@@ -20,6 +16,8 @@ ls /dt7/usr/include/x86_64-linux-gnu/
 # This really shouldn't be required to get 3.7 builds working
 ln -sf /usr/include/x86_64-linux-gnu/python3.7m /dt7/usr/include/x86_64-linux-gnu/python3.7m || true
 ln -sf /usr/include/x86_64-linux-gnu/python3.7m /dt8/usr/include/x86_64-linux-gnu/python3.7m || true
+ln -sf /usr/include/x86_64-linux-gnu/python3.8 /dt7/usr/include/x86_64-linux-gnu/python3.8 || true
+ln -sf /usr/include/x86_64-linux-gnu/python3.8 /dt8/usr/include/x86_64-linux-gnu/python3.8 || true
 
 curl https://bootstrap.pypa.io/get-pip.py | python
 python --version
