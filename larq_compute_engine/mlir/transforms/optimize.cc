ad188e448878341973d2ad273bf1ed8641d0f589
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-05-22 01:07:15 +0100

    Fix bitpacked activation pass when interacting with fused BN/activation. (#372)
    * Fix bitpacked activation pass when interacting with fused BN/activation.
    
    Closes #371.
    
    * Review suggestion.
    
    * Review suggestion.

--- larq_compute_engine/mlir/transforms/optimize.cc
+++ larq_compute_engine/mlir/transforms/optimize.cc
@@ -1,5 +1,6 @@
 #include "larq_compute_engine/core/packbits.h"
 #include "larq_compute_engine/mlir/ir/lce_ops.h"
+#include "llvm/ADT/Optional.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
@@ -68,123 +69,73 @@ DenseElementsAttr Bitpack(PatternRewriter& builder, Attribute x) {
 
 #include "larq_compute_engine/mlir/transforms/generated_optimize.inc"
 
-struct SetBconvReadWriteBitpacked : public OpRewritePattern<TF::LceBconv2dOp> {
-  using OpRewritePattern<TF::LceBconv2dOp>::OpRewritePattern;
+llvm::Optional<RankedTensorType> maybeGetBitpackedType(
+    PatternRewriter& rewriter, ShapedType existing_type) {
+  if (existing_type.getElementType().isInteger(32)) return llvm::None;
 
-  LogicalResult matchAndRewrite(TF::LceBconv2dOp outer_bconv_op,
+  const auto existing_shape = existing_type.getShape();
+  if (existing_shape.size() != 4) return llvm::None;
+
+  const auto channels = existing_shape[3];
+  const auto packed_channels = (channels + 32 - 1) / 32;
+  return RankedTensorType::get({existing_shape[0], existing_shape[1],
+                                existing_shape[2], packed_channels},
+                               rewriter.getIntegerType(32));
+}
+
+template <typename BinaryOp>
+struct SetBitpackedActivations : public OpRewritePattern<BinaryOp> {
+  using OpRewritePattern<BinaryOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(BinaryOp outer_binary_op,
                                 PatternRewriter& rewriter) const override {
-    /*************
-     * Match ops *
-     *************/
+    Operation* input_op = outer_binary_op.input().getDefiningOp();
 
-    auto intermediate_maxpool_op = dyn_cast_or_null<TFL::MaxPool2DOp>(
-        outer_bconv_op.input().getDefiningOp());
+    // If the input op has more than one use, we can't apply an optimisation.
+    if (!input_op || !input_op->hasOneUse()) return failure();
 
-    if (intermediate_maxpool_op &&
-        intermediate_maxpool_op.fused_activation_function() != "NONE") {
-      return failure();
-    }
-
-    auto inner_bconv_op =
-        intermediate_maxpool_op
-            ? dyn_cast_or_null<TF::LceBconv2dOp>(
-                  intermediate_maxpool_op.input().getDefiningOp())
-            : dyn_cast_or_null<TF::LceBconv2dOp>(
-                  outer_bconv_op.input().getDefiningOp());
-
-    if (inner_bconv_op && inner_bconv_op.padding() == "SAME" &&
-        inner_bconv_op.pad_values() != 1) {
-      // We don't return `failure()` here because we still want to match the
-      // pattern `binary maxpool -> binary conv`.
-      inner_bconv_op = nullptr;
-    }
-
-    if (!inner_bconv_op && !intermediate_maxpool_op) return failure();
-
-    if (!outer_bconv_op.input().hasOneUse() ||
-        (intermediate_maxpool_op && inner_bconv_op &&
-         !intermediate_maxpool_op.input().hasOneUse())) {
-      return failure();
-    }
-
-    constexpr int bitwidth = 32;  // We use 32-bit bitpacking.
-
-    /*********************
-     * Compute new types *
-     *********************/
-
-    RankedTensorType new_inner_bconv_op_type;
-    RankedTensorType new_intermediate_maxpool_op_type;
-
-    // If applicable, compute the new type for the inner bconv op.
+    // Try and match `input_op` to a binary convolution.
+    auto inner_bconv_op = dyn_cast_or_null<TF::LceBconv2dOp>(input_op);
     if (inner_bconv_op) {
-      ShapedType inner_bconv_op_type =
-          inner_bconv_op.getType().cast<ShapedType>();
-
-      // Don't apply this transformation if the type is already I32.
-      if (inner_bconv_op_type.getElementType().isInteger(bitwidth)) {
+      if (inner_bconv_op.padding() == "SAME" &&
+          inner_bconv_op.pad_values() != 1) {
         return failure();
       }
 
-      // Compute the new type for the inner bconv op.
-      const auto inner_bconv_op_shape = inner_bconv_op_type.getShape();
-      if (inner_bconv_op_shape.size() != 4) return failure();
-      const auto channels = inner_bconv_op_shape[3];
-      const auto packed_channels = (channels + bitwidth - 1) / bitwidth;
-      new_inner_bconv_op_type = RankedTensorType::get(
-          {inner_bconv_op_shape[0], inner_bconv_op_shape[1],
-           inner_bconv_op_shape[2], packed_channels},
-          rewriter.getIntegerType(bitwidth));
+      if (auto maybe_bitpacked_type = maybeGetBitpackedType(
+              rewriter, inner_bconv_op.getType().cast<ShapedType>())) {
+        rewriter.replaceOpWithNewOp<TF::LceBconv2dOp>(
+            inner_bconv_op, *maybe_bitpacked_type, inner_bconv_op.getOperands(),
+            inner_bconv_op.getAttrs());
+        return success();
+      }
     }
 
-    // If applicable, compute the new type for the intermediate maxpool op.
-    if (intermediate_maxpool_op) {
-      ShapedType intermediate_maxpool_op_type =
-          intermediate_maxpool_op.getType().cast<ShapedType>();
-
-      // Don't apply this transformation if the type is already I32.
-      if (intermediate_maxpool_op_type.getElementType().isInteger(bitwidth)) {
+    // Otherwise, try and match `input_op` to a maxpool.
+    auto maxpool_op = dyn_cast_or_null<TFL::MaxPool2DOp>(input_op);
+    if (maxpool_op) {
+      if (maxpool_op.fused_activation_function() != "NONE") {
         return failure();
       }
 
-      const auto intermediate_maxpool_op_shape =
-          intermediate_maxpool_op_type.getShape();
-      if (intermediate_maxpool_op_shape.size() != 4) return failure();
-      const auto channels = intermediate_maxpool_op_shape[3];
-      const auto packed_channels = (channels + bitwidth - 1) / bitwidth;
-      new_intermediate_maxpool_op_type = RankedTensorType::get(
-          {intermediate_maxpool_op_shape[0], intermediate_maxpool_op_shape[1],
-           intermediate_maxpool_op_shape[2], packed_channels},
-          rewriter.getIntegerType(bitwidth));
+      if (auto bitpacked_type = maybeGetBitpackedType(
+              rewriter, maxpool_op.getType().cast<ShapedType>())) {
+        rewriter.replaceOpWithNewOp<TF::LceBMaxPool2dOp>(
+            maxpool_op, *bitpacked_type, maxpool_op.input(),
+            rewriter.getStringAttr(maxpool_op.padding()),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(32),
+                                    maxpool_op.stride_h()),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(32),
+                                    maxpool_op.stride_w()),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(32),
+                                    maxpool_op.filter_width()),
+            rewriter.getIntegerAttr(rewriter.getIntegerType(32),
+                                    maxpool_op.filter_height()));
+        return success();
+      }
     }
 
-    /***************************
-     * Perform op replacements *
-     ***************************/
-
-    // We have to replace the inner-bconv op first...
-    if (inner_bconv_op) {
-      rewriter.replaceOpWithNewOp<TF::LceBconv2dOp>(
-          inner_bconv_op, new_inner_bconv_op_type, inner_bconv_op.getOperands(),
-          inner_bconv_op.getAttrs());
-    }
-    // ...then the maxpool.
-    if (intermediate_maxpool_op) {
-      rewriter.replaceOpWithNewOp<TF::LceBMaxPool2dOp>(
-          intermediate_maxpool_op, new_intermediate_maxpool_op_type,
-          intermediate_maxpool_op.input(),
-          rewriter.getStringAttr(intermediate_maxpool_op.padding()),
-          rewriter.getIntegerAttr(rewriter.getIntegerType(32),
-                                  intermediate_maxpool_op.stride_h()),
-          rewriter.getIntegerAttr(rewriter.getIntegerType(32),
-                                  intermediate_maxpool_op.stride_w()),
-          rewriter.getIntegerAttr(rewriter.getIntegerType(32),
-                                  intermediate_maxpool_op.filter_width()),
-          rewriter.getIntegerAttr(rewriter.getIntegerType(32),
-                                  intermediate_maxpool_op.filter_height()));
-    }
-
-    return success();
+    return failure();
   };
 };
 
@@ -195,7 +146,8 @@ void OptimizeLCE::runOnFunction() {
 
   TFL::populateWithGenerated(ctx, &patterns);
   if (experimental_enable_bitpacked_activations_) {
-    patterns.insert<SetBconvReadWriteBitpacked>(ctx);
+    patterns.insert<SetBitpackedActivations<TF::LceBconv2dOp>>(ctx);
+    patterns.insert<SetBitpackedActivations<TF::LceBMaxPool2dOp>>(ctx);
   }
   applyPatternsAndFoldGreedily(func, patterns);
 }
