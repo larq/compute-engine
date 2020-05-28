6743057ea21ca08ccf320e2d97f0526341d47350
Lukas Geiger <lukas.geiger94@gmail.com>
2020-05-28 18:58:59 +0100

    Move buildCustomOptions to op definition

--- larq_compute_engine/mlir/transforms/legalize_tflite.cc
+++ larq_compute_engine/mlir/transforms/legalize_tflite.cc
@@ -1,4 +1,3 @@
-#include "flatbuffers/flexbuffers.h"
 #include "larq_compute_engine/mlir/ir/lce_ops.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
@@ -8,39 +7,6 @@ namespace mlir {
 namespace TFL {
 
 namespace {
-
-std::vector<uint8_t> buildCustomOption(TF::BsignOp op) { return {}; }
-
-std::vector<uint8_t> buildCustomOption(TF::Bconv2dOp op) {
-  flexbuffers::Builder fbb;
-  fbb.Map([&]() {
-    fbb.Int("channels_in", op.channels_in().getSExtValue());
-    fbb.Int("dilation_height_factor",
-            op.dilation_height_factor().getSExtValue());
-    fbb.Int("dilation_width_factor", op.dilation_width_factor().getSExtValue());
-    fbb.String("fused_activation_function",
-               std::string(op.fused_activation_function()));
-    fbb.Int("pad_values", op.pad_values().getSExtValue());
-    fbb.String("padding", std::string(op.padding()));
-    fbb.Int("stride_height", op.stride_height().getSExtValue());
-    fbb.Int("stride_width", op.stride_width().getSExtValue());
-  });
-  fbb.Finish();
-  return fbb.GetBuffer();
-}
-
-std::vector<uint8_t> buildCustomOption(TF::BMaxPool2dOp op) {
-  flexbuffers::Builder fbb;
-  fbb.Map([&]() {
-    fbb.String("padding", std::string(op.padding()));
-    fbb.Int("stride_width", op.stride_width().getSExtValue());
-    fbb.Int("stride_height", op.stride_height().getSExtValue());
-    fbb.Int("filter_width", op.filter_width().getSExtValue());
-    fbb.Int("filter_height", op.filter_height().getSExtValue());
-  });
-  fbb.Finish();
-  return fbb.GetBuffer();
-}
 
 struct LegalizeLCE : public PassWrapper<LegalizeLCE, FunctionPass> {
   void runOnFunction() override;
@@ -52,7 +18,7 @@ struct LegalizeToCustomOp : public OpRewritePattern<LarqOp> {
 
   LogicalResult matchAndRewrite(LarqOp larq_op,
                                 PatternRewriter& rewriter) const override {
-    std::vector<uint8_t> options = buildCustomOption(larq_op);
+    std::vector<uint8_t> options = larq_op.buildCustomOptions();
     Operation* op = larq_op.getOperation();
     ShapedType type = RankedTensorType::get(
         {static_cast<int64_t>(options.size())}, rewriter.getIntegerType(8));
