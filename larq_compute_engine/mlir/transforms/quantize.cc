e1bdca680ed2bfac4ab3b373b3a0d6b062f8cb06
Lukas Geiger <lukas.geiger94@gmail.com>
2020-06-09 21:45:03 +0100

    Fix trailing quantization op

--- larq_compute_engine/mlir/transforms/quantize.cc
+++ larq_compute_engine/mlir/transforms/quantize.cc
@@ -1,4 +1,4 @@
-// This transformation pass applies hybrid quantization on TFLite dialect.
+// This transformation pass applies quantization on Larq dialect.
 
 #include "larq_compute_engine/mlir/ir/lce_ops.h"
 #include "mlir/Dialect/Quant/QuantTypes.h"
@@ -24,46 +24,29 @@ namespace TFL {
 //
 namespace {
 
-// Integer quantization rewrite pattern for TFLite.
-// We allow hybrid operands so post_activation_multiplier and
-// post_activation_bias can be kept in float32 for now
-struct TFLHybridQuantization
-    : public quant::QuantizationPattern<TFLHybridQuantization, QuantizeOp,
-                                        DequantizeOp, NumericVerifyOp> {
-  explicit TFLHybridQuantization(MLIRContext* ctx, bool verify_numeric,
-                                 float tolerance, bool verify_single_layer)
-      : BaseType(ctx, verify_numeric, tolerance, verify_single_layer) {}
-  static bool AllowHybridOperand() { return true; }
-  static bool AllowHybridResult() { return false; }
-};
-
 // Applies quantization on the model in TFL dialect.
-struct HybridQuantizePass
-    : public PassWrapper<HybridQuantizePass, FunctionPass> {
+struct LCEQuantizePass : public PassWrapper<LCEQuantizePass, FunctionPass> {
   void runOnFunction() override;
 };
 
 #include "larq_compute_engine/mlir/transforms/generated_quantize.inc"
 
-void HybridQuantizePass::runOnFunction() {
+void LCEQuantizePass::runOnFunction() {
   OwningRewritePatternList patterns;
   auto func = getFunction();
   auto* ctx = func.getContext();
   TFL::populateWithGenerated(ctx, &patterns);
-  patterns.insert<TFLHybridQuantization>(ctx, /*enable_numeric_verify=*/false,
-                                         /*error_tolerance=*/5.0,
-                                         /*enable_single_layer_verify=*/true);
   applyPatternsAndFoldGreedily(func, patterns);
 }
 }  // namespace
 
 // Creates an instance of the TensorFlow Lite dialect QuantizeTFL pass.
-std::unique_ptr<OperationPass<FuncOp>> CreateHybridQuantizePass() {
-  return std::make_unique<HybridQuantizePass>();
+std::unique_ptr<OperationPass<FuncOp>> CreateLCEQuantizePass() {
+  return std::make_unique<LCEQuantizePass>();
 }
 
-static PassRegistration<HybridQuantizePass> pass(
-    "tfl-hybrid-quantize",
+static PassRegistration<LCEQuantizePass> pass(
+    "lce-quantize",
     "Apply hybrid quantization on models in TensorFlow Lite dialect");
 
 }  // namespace TFL
