7d9046af56b0f277771ac676390a9e7540650241
Lukas Geiger <lukas.geiger94@gmail.com>
2020-06-09 22:20:30 +0100

    Remove unused imports

--- larq_compute_engine/mlir/transforms/quantize.cc
+++ larq_compute_engine/mlir/transforms/quantize.cc
@@ -1,20 +1,9 @@
 // This transformation pass applies quantization on Larq dialect.
 
 #include "larq_compute_engine/mlir/ir/lce_ops.h"
-#include "mlir/Dialect/Quant/QuantTypes.h"
-#include "mlir/IR/Attributes.h"
-#include "mlir/IR/Builders.h"
-#include "mlir/IR/MLIRContext.h"
-#include "mlir/IR/Matchers.h"
-#include "mlir/IR/Module.h"
-#include "mlir/IR/Operation.h"
-#include "mlir/IR/OperationSupport.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "tensorflow/compiler/mlir/lite/ir/tfl_ops.h"
-#include "tensorflow/compiler/mlir/lite/quantization/quantization_utils.h"
-#include "tensorflow/compiler/mlir/lite/transforms/passes.h"
-#include "tensorflow/compiler/mlir/lite/utils/validators.h"
 
 namespace mlir {
 namespace TFL {
