8a81c64011eabc6470973b3e1c2d11142731346a
Lukas Geiger <lukas.geiger94@gmail.com>
2020-07-22 15:23:38 +0200

    Remove unused packbits_arm32 (#437)
    * :fire: packbits_arm32
    
    * Remove missing tests from arm32 test script

--- larq_compute_engine/tests/cc_tests_arm32_qemu.sh
+++ larq_compute_engine/tests/cc_tests_arm32_qemu.sh
@@ -4,11 +4,6 @@ set -e
 
 export QEMU_LD_PREFIX="external/arm_compiler/arm-rpi-linux-gnueabihf/sysroot"
 
-for testfile in larq_compute_engine/core/tests/*_tests;
-do
-    qemu-arm "$testfile"
-done
-
 for testfile in larq_compute_engine/tflite/tests/*_test;
 do
     qemu-arm "$testfile"
