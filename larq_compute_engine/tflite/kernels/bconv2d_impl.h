954db2c7c937ecf764d44614648ed431487cb247
Lukas Geiger <lukas.geiger94@gmail.com>
2020-07-27 15:29:45 +0200

    WIP: optimize Im2Col

--- larq_compute_engine/tflite/kernels/bconv2d_impl.h
+++ larq_compute_engine/tflite/kernels/bconv2d_impl.h
@@ -1,6 +1,8 @@
 #ifndef COMPUTE_EGNINE_TFLITE_KERNELS_BCONV_2D_IMPL_H_
 #define COMPUTE_EGNINE_TFLITE_KERNELS_BCONV_2D_IMPL_H_
 
+#include <cassert>
+
 #include "larq_compute_engine/core/bgemm_impl.h"
 #include "larq_compute_engine/core/packbits.h"
 #include "larq_compute_engine/core/packbits_utils.h"
@@ -12,8 +14,194 @@
 #include "tensorflow/lite/kernels/internal/types.h"
 #include "tensorflow/lite/kernels/padding.h"
 
-using namespace tflite;
+namespace tflite {
+namespace wip_ops {
+template <typename T>
+inline void ExtractPatchIntoBufferColumn(const RuntimeShape& input_shape, int w,
+                                         int h, int b, int kheight, int kwidth,
+                                         int stride_width, int stride_height,
+                                         int pad_width, int pad_height,
+                                         int in_width, int in_height,
+                                         int in_depth, int single_buffer_length,
+                                         int buffer_id, const T* in_data,
+                                         T* conv_buffer_data, uint8 zero_byte) {
+  ruy::profiler::ScopeLabel label("ExtractPatchIntoBufferColumn");
+  TFLITE_DCHECK_EQ(input_shape.DimensionsCount(), 4);
+  // This chunk of code reshapes all the inputs corresponding to
+  // output (b, h, w) to a column vector in conv_buffer(:, buffer_id).
+  const int kwidth_times_indepth = kwidth * in_depth;     // Could be move out
+  const int inwidth_times_indepth = in_width * in_depth;  // Could be move out
+  const int ih_ungated_start = h * stride_height - pad_height;
+  const int ih_ungated_end = (ih_ungated_start + kheight);
+  const int ih_end = std::min(ih_ungated_end, in_height);
+  const int iw_ungated_start = w * stride_width - pad_width;
+  const int iw_ungated_end = (iw_ungated_start + kwidth);
+  const int iw_end = std::min(iw_ungated_end, in_width);
+  // If the patch is off the edge of the input image, skip writing those rows
+  // and columns from the patch into the output array.
+  const int h_offset = std::max(0, -ih_ungated_start);
+  const int w_offset = std::max(0, -iw_ungated_start);
+  const int ih_start = std::max(0, ih_ungated_start);
+  const int iw_start = std::max(0, iw_ungated_start);
+  const int single_row_num =
+      std::min(kwidth - w_offset, in_width - iw_start) * in_depth;
+  const int output_row_offset = (buffer_id * single_buffer_length);
+  int out_offset =
+      output_row_offset + (h_offset * kwidth + w_offset) * in_depth;
+  int in_offset = Offset(input_shape, b, ih_start, iw_start, 0);
 
+  // Express all of the calculations as padding around the input patch.
+  const int top_padding = h_offset;
+  const int bottom_padding = (ih_ungated_end - ih_end);
+  const int left_padding = w_offset;
+  const int right_padding = (iw_ungated_end - iw_end);
+  assert(single_row_num ==
+         ((kwidth - (left_padding + right_padding)) * in_depth));
+
+  // Write out zeroes to the elements representing the top rows of the input
+  // patch that are off the edge of the input image.
+  if (top_padding > 0) {
+    const int top_row_elements = (top_padding * kwidth * in_depth);
+    memset(conv_buffer_data + output_row_offset, zero_byte,
+           (top_row_elements * sizeof(T)));
+  }
+
+  // If the patch is on the interior of the input image horizontally, just copy
+  // over the rows sequentially, otherwise add zero padding at the start or end.
+  if (left_padding > 0) {
+    if (right_padding > 0) {
+      for (int ih = ih_start; ih < ih_end; ++ih) {
+        const int left_start = (out_offset - (left_padding * in_depth));
+        const int right_start = (out_offset + single_row_num);
+        memset(conv_buffer_data + left_start, zero_byte,
+               (left_padding * in_depth * sizeof(T)));
+        memcpy(conv_buffer_data + out_offset, in_data + in_offset,
+               single_row_num * sizeof(T));
+        memset(conv_buffer_data + right_start, zero_byte,
+               (right_padding * in_depth * sizeof(T)));
+        out_offset += kwidth_times_indepth;
+        in_offset += inwidth_times_indepth;
+      }
+    } else {
+      for (int ih = ih_start; ih < ih_end; ++ih) {
+        const int left_start = (out_offset - (left_padding * in_depth));
+        memset(conv_buffer_data + left_start, zero_byte,
+               (left_padding * in_depth * sizeof(T)));
+        memcpy(conv_buffer_data + out_offset, in_data + in_offset,
+               single_row_num * sizeof(T));
+        out_offset += kwidth_times_indepth;
+        in_offset += inwidth_times_indepth;
+      }
+    }
+  } else {
+    if (right_padding > 0) {
+      for (int ih = ih_start; ih < ih_end; ++ih) {
+        const int right_start = (out_offset + single_row_num);
+        memcpy(conv_buffer_data + out_offset, in_data + in_offset,
+               single_row_num * sizeof(T));
+        memset(conv_buffer_data + right_start, zero_byte,
+               (right_padding * in_depth * sizeof(T)));
+        out_offset += kwidth_times_indepth;
+        in_offset += inwidth_times_indepth;
+      }
+    } else {
+      for (int ih = ih_start; ih < ih_end; ++ih) {
+        memcpy(conv_buffer_data + out_offset, in_data + in_offset,
+               single_row_num * sizeof(T));
+        out_offset += kwidth_times_indepth;
+        in_offset += inwidth_times_indepth;
+      }
+    }
+  }
+
+  // If the bottom of the patch falls off the input image, pad the values
+  // representing those input rows with zeroes.
+  if (bottom_padding > 0) {
+    const int bottom_row_elements = (bottom_padding * kwidth * in_depth);
+    const int bottom_start =
+        output_row_offset +
+        ((top_padding + (ih_end - ih_start)) * kwidth * in_depth);
+    memset(conv_buffer_data + bottom_start, zero_byte,
+           (bottom_row_elements * sizeof(T)));
+  }
+}
+
+template <typename T>
+void Im2col(const ConvParams& params, int kheight, int kwidth, uint8 zero_byte,
+            const RuntimeShape& input_shape, const T* input_data,
+            const RuntimeShape& output_shape, T* output_data) {
+  ruy::profiler::ScopeLabel label("Im2col");
+  const int stride_width = params.stride_width;
+  const int stride_height = params.stride_height;
+  const int pad_width = params.padding_values.width;
+  const int pad_height = params.padding_values.height;
+  TFLITE_DCHECK_EQ(input_shape.DimensionsCount(), 4);
+  TFLITE_DCHECK_EQ(output_shape.DimensionsCount(), 4);
+
+  const int batches = MatchingDim(input_shape, 0, output_shape, 0);
+  const int input_depth = input_shape.Dims(3);
+  const int input_width = input_shape.Dims(2);
+  const int input_height = input_shape.Dims(1);
+  const int output_depth = output_shape.Dims(3);
+  const int output_width = output_shape.Dims(2);
+  const int output_height = output_shape.Dims(1);
+
+  int buffer_id = 0;
+  // Loop over the output nodes.
+  for (int b = 0; b < batches; ++b) {
+    for (int h = 0; h < output_height; ++h) {
+      for (int w = 0; w < output_width; ++w) {
+        ExtractPatchIntoBufferColumn(
+            input_shape, w, h, b, kheight, kwidth, stride_width, stride_height,
+            pad_width, pad_height, input_width, input_height, input_depth,
+            output_depth, buffer_id, input_data, output_data, zero_byte);
+        ++buffer_id;
+      }
+    }
+  }
+}
+
+template <typename T>
+void Im2col(const ConvParams& params, int kheight, int kwidth,
+            const int32_t* input_offsets, const int input_offsets_size,
+            const RuntimeShape& input_shape, const T* input_data,
+            const RuntimeShape& output_shape, T* output_data) {
+  ruy::profiler::ScopeLabel label("Im2col");
+  const int stride_width = params.stride_width;
+  const int stride_height = params.stride_height;
+  const int pad_width = params.padding_values.width;
+  const int pad_height = params.padding_values.height;
+  TFLITE_DCHECK_EQ(input_shape.DimensionsCount(), 4);
+  TFLITE_DCHECK_EQ(output_shape.DimensionsCount(), 4);
+
+  const int batches = MatchingDim(input_shape, 0, output_shape, 0);
+  TFLITE_DCHECK_EQ(batches, input_offsets_size);
+  const int input_depth = input_shape.Dims(3);
+  const int input_width = input_shape.Dims(2);
+  const int input_height = input_shape.Dims(1);
+  const int output_depth = output_shape.Dims(3);
+  const int output_width = output_shape.Dims(2);
+  const int output_height = output_shape.Dims(1);
+
+  int buffer_id = 0;
+  // Loop over the output nodes.
+  for (int b = 0; b < batches; ++b) {
+    uint8_t zero_byte = static_cast<uint8_t>(input_offsets[b]);
+    for (int h = 0; h < output_height; ++h) {
+      for (int w = 0; w < output_width; ++w) {
+        ExtractPatchIntoBufferColumn(
+            input_shape, w, h, b, kheight, kwidth, stride_width, stride_height,
+            pad_width, pad_height, input_width, input_height, input_depth,
+            output_depth, buffer_id, input_data, output_data, zero_byte);
+        ++buffer_id;
+      }
+    }
+  }
+}
+}  // namespace wip_ops
+}  // namespace tflite
+
+using namespace tflite;
 namespace compute_engine {
 
 namespace ce = compute_engine;
@@ -50,9 +238,8 @@ inline void im2col(const ConvParams& params, const RuntimeShape& input_shape,
     shape = &im2col_shape;
   } else if (need_im2col) {
     TF_LITE_ASSERT(im2col_data);
-    optimized_ops::Im2col<T>(params, filter_height, filter_width, zero_byte,
-                             input_shape, input_data, im2col_shape,
-                             im2col_data);
+    wip_ops::Im2col<T>(params, filter_height, filter_width, zero_byte,
+                       input_shape, input_data, im2col_shape, im2col_data);
     *result_data = im2col_data;
     shape = &im2col_shape;
   } else {
