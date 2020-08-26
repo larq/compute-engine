fdb7f55d93c7bd2607a3401dd1f480d834ec8574
Tom Bannink <tombannink@gmail.com>
2020-08-26 20:05:06 +0200

    WIP: Update Makefile build system

--- larq_compute_engine/tflite/build_make/build_lce.sh
+++ larq_compute_engine/tflite/build_make/build_lce.sh
@@ -3,7 +3,7 @@ set -e
 
 usage()
 {
-    echo "Usage: build_lqce.sh [--native] [--rpi] [--ios] [--aarch64] [--benchmark] [--clean] [--pip]
+    echo "Usage: build_lqce.sh [--native] [--rpi] [--ios] [--aarch64] [--benchmark] [--clean]
 
 --native        Build for the host platform
 --rpi           Cross-compile for Raspberry Pi (32-bit armv7)
@@ -17,14 +17,11 @@ The --rpi option requires the arm-linux-gnueabihf toolchain.
 The --aarch64 option requires the aarch64-linux-gnu toolchain.
 The --ios option requires the iOS SDK.
 
---benchmark     Compile with gemmlowp profiling enabled
+--benchmark     Compile with RUY profiling enabled
 --clean         Delete intermediate build files
---pip           Also build the pip package
 
 If doing a benchmark build when you have previously built without --benchmark
-then you should pass --clean to do a complete rebuild.
-
-Building the pip package is only available for the --native target."
+then you should pass --clean to do a complete rebuild."
 }
 
 
@@ -39,7 +36,6 @@ ios=0
 aarch64=0
 benchmark=0
 clean=0
-pip=0
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -60,9 +56,6 @@ while [ "$1" != "" ]; do
             ;;
         -c | --clean)
             clean=1
-            ;;
-        --pip)
-	    pip=1
             ;;
         -h | --help )
             usage
@@ -95,9 +88,9 @@ elif [ "$UNAME" = "Darwin" ] ; then
 fi
 HOST_ARCH="$(if uname -m | grep -q i[345678]86; then echo x86_32; else uname -m; fi)"
 
-export EXTRA_CXXFLAGS="-DTFLITE_WITH_RUY"
+export BUILD_WITH_RUY=true
 if [ "$benchmark" == "1" ]; then
-    export EXTRA_CXXFLAGS="${EXTRA_CXXFLAGS} -DGEMMLOWP_PROFILING"
+    export BUILD_WITH_RUY_PROFILER=true
 fi
 
 if [ "$clean" == "1" ]; then
@@ -112,18 +105,11 @@ fi
 
 if [ "$native" == "1" ]; then
     echo " --> native build"
-    # Build tflite (will automatically skip when up-to-date
+    # Build the tflite lib (will automatically skip when up-to-date
     # this line is taken from "tensorflow/lite/tools/make/build_lib.sh"
     make -j ${NUM_HYPERTHREADS} BUILD_WITH_NNAPI=false -C "${TF_DIR}" -f tensorflow/lite/tools/make/Makefile
-    # Build compute-engine kernels
+    # Build compute-engine kernels and benchmark binary
     make -j ${NUM_HYPERTHREADS} BUILD_WITH_NNAPI=false -C "${ROOT_DIR}" -f ${LCE_MAKEFILE}
-    # Rebuild tflite binaries such as benchmarking libs to include our ops
-    ${TF_DIR}/tensorflow/lite/tools/make/build_lib.sh
-
-    # Optionally build the pip package
-    if [ "$pip" == "1" ]; then
-        ${TF_DIR}/tensorflow/lite/tools/pip_package/build_pip_package.sh
-    fi
 fi
 
 if [ "$rpi" == "1" ]; then
@@ -131,17 +117,8 @@ if [ "$rpi" == "1" ]; then
     # Stored in gen/rpi_armv7l
     # This line is taken form "tensorflow/lite/tools/make/build_rpi_lib.sh"
     make -j ${NUM_HYPERTHREADS} TARGET=rpi -C "${TF_DIR}" -f tensorflow/lite/tools/make/Makefile
-    # Build compute-engine kernels
+    # Build compute-engine kernels and benchmark binary
     make -j ${NUM_HYPERTHREADS} TARGET=rpi -C "${ROOT_DIR}" -f ${LCE_MAKEFILE}
-    ${TF_DIR}/tensorflow/lite/tools/make/build_rpi_lib.sh
-
-    # Optionally build the pip package
-    if [ "$pip" == "1" ]; then
-        # This will only work if we are running this natively on a raspberry pi
-        if [[ $HOST_ARCH == armv7* ]]; then
-            ${TF_DIR}/tensorflow/lite/tools/pip_package/build_pip_package.sh
-        fi
-    fi
 fi
 
 if [ "$ios" == "1" ]; then
@@ -157,7 +134,6 @@ if [ "$ios" == "1" ]; then
         # Stored in gen/ios_$arch
         make -j ${NUM_HYPERTHREADS} TARGET=ios TARGET_ARCH=${arch} -C "${ROOT_DIR}" -f ${LCE_MAKEFILE}
     done
-    ${TF_DIR}/tensorflow/lite/tools/make/build_ios_universal_lib.sh $profiling_opt
 fi
 
 if [ "$aarch64" == "1" ]; then
@@ -165,7 +141,6 @@ if [ "$aarch64" == "1" ]; then
     # Stored in gen/aarch64_armv8-a
     # This line is taken from "tensorflow/lite/tools/make/build_aarch64_lib.sh"
     make -j ${NUM_HYPERTHREADS} TARGET=aarch64 -C "${TF_DIR}" -f tensorflow/lite/tools/make/Makefile
-    # Build compute-engine kernels
+    # Build compute-engine kernels and benchmark binary
     make -j ${NUM_HYPERTHREADS} TARGET=aarch64 -C "${ROOT_DIR}" -f ${LCE_MAKEFILE}
-    ${TF_DIR}/tensorflow/lite/tools/make/build_aarch64_lib.sh
 fi
