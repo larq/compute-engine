fe617cf465c86a62448864be83d43b29494873d4
Lukas Geiger <lukas.geiger94@gmail.com>
2020-08-27 11:09:54 +0200

    Update examples/lce_minimal.cc
    Co-authored-by: Tom Bannink <tom@plumerai.com>

--- examples/lce_minimal.cc
+++ examples/lce_minimal.cc
@@ -8,7 +8,7 @@
 
 // This file is based on the TF lite minimal example where the
 // "BuiltinOpResolver" is modified to include the "Larq Compute Engine" custom
-// ops. Here we read a binary model form disk and perform inference by using the
+// ops. Here we read a binary model from disk and perform inference by using the
 // C++ interface. See the BUILD file in this directory to see an example of
 // linking "Larq Compute Engine" cutoms ops to your inference binary.
 
