larq/compute-engine

commit 346f72e3ab575ee65af90b025bd234c8b01d1815

Fuse part of the back-transformation into the multiplier and bias. (#490)
The back-transformation involves a left-shift and a subtraction. Fuse
the subtraction into the existing output transform float32 multiplier
and bias, so that the subtract instructions can be removed from the
optimised Arm32 and Aarch64 kernels.

larq_compute_engine/core/bgemm_kernels_arm64.h
@@ -83,7 +83,6 @@ using compute_engine::core::TBitpacked;
 #define RUY_OFFSET_DEPTH 76
 #define RUY_OFFSET_CLAMP_MIN 80
 #define RUY_OFFSET_CLAMP_MAX 84
-#define RUY_OFFSET_BACKTRANSFORM_ADD 88
 
 template <typename Params>
 void CheckOffsetsInKernelParams(const Params&) {
@@ -106,8 +105,6 @@ void CheckOffsetsInKernelParams(const Params&) {
   static_assert(offsetof(Params, depth) == RUY_OFFSET_DEPTH, "");
   static_assert(offsetof(Params, clamp_min) == RUY_OFFSET_CLAMP_MIN, "");
   static_assert(offsetof(Params, clamp_max) == RUY_OFFSET_CLAMP_MAX, "");
-  static_assert(
-      offsetof(Params, backtransform_add) == RUY_OFFSET_BACKTRANSFORM_ADD, "");
 }
 
 // clang-format off
@@ -292,10 +289,6 @@ void BinaryKernelNeonOutOfOrder4x4(const BinaryKernelParams<4, 4>& params) {
       "mov %[lhs_ptr], %[lhs_col_ptr]\n"
       "mov %[rhs_ptr], %[rhs_col_ptr]\n"
 
-      // Load backtransform add (duplicate 4 times into v13)
-      "ldr w1, [%[params], #" RUY_STR(RUY_OFFSET_BACKTRANSFORM_ADD) "]\n"
-      "dup v13.4s, w1 \n"
-
       // Load multiplication bias
       "ldr x1, [%[params], #" RUY_STR(RUY_OFFSET_POST_ACTIVATION_MULTIPLIER) "]\n"
       // Offset these base pointers as needed given the current row, col.
@@ -479,7 +472,6 @@ void BinaryKernelNeonOutOfOrder4x4(const BinaryKernelParams<4, 4>& params) {
         "v26", "v27", "v28", "v29", "v30", "v31");
 }
 
-#undef RUY_OFFSET_BACKTRANSFORM_ADD
 #undef RUY_OFFSET_POST_ACTIVATION_MULTIPLIER
 #undef RUY_OFFSET_POST_ACTIVATION_BIAS
 #undef RUY_OFFSET_LHS_BASE_PTR
@@ -616,7 +608,6 @@ void BinaryKernelNeonOutOfOrder4x4(const BinaryKernelParams<4, 4>& params) {
 #define RUY_OFFSET_DEPTH 76
 #define RUY_OFFSET_CLAMP_MIN 80
 #define RUY_OFFSET_CLAMP_MAX 84
-#define RUY_OFFSET_BACKTRANSFORM_ADD 88
 
 // clang-format off
 
@@ -768,9 +759,6 @@ void BinaryKernelNeonOutOfOrder8x4(const BinaryKernelParams<8, 4>& params) {
       // anymore in the rest of the work on the current block. We do it in
       // parallel with the back-transform shift.
 
-      // Load the backtransform add.
-      "ldr w1, [%[params], #" RUY_STR(RUY_OFFSET_BACKTRANSFORM_ADD) "]\n"
-
       // Load the `clamp_min` bound.
       "ldr w2, [%[params], #" RUY_STR(RUY_OFFSET_CLAMP_MIN) "]\n"
 
@@ -780,8 +768,8 @@ void BinaryKernelNeonOutOfOrder8x4(const BinaryKernelParams<8, 4>& params) {
       // Perform the back-transformation shift with 'unsigned shift left long'
       // instructions; this performs the necessary left shift and extends the
       // result, so we get a int16 -> int32 transformation for free.
+      "dup v29.4s, w2\n"  // In parallel, duplicate `clamp_min` into v29.
       "ushll v20.4s, v24.4h, #1\n"
-      "dup v28.4s, w1\n"  // In parallel, duplicate `backtransform_add` into v28.
       "ushll2 v21.4s, v24.8h, #1\n"
       "ld1 {v8.4s}, [%[rhs_ptr]], #16\n"
       "ushll v22.4s, v25.4h, #1\n"
@@ -792,17 +780,6 @@ void BinaryKernelNeonOutOfOrder8x4(const BinaryKernelParams<8, 4>& params) {
       "ushll v26.4s, v27.4h, #1\n"
       "ushll2 v27.4s, v27.8h, #1\n"
       "ld1 {v4.4s, v5.4s, v6.4s, v7.4s}, [%[lhs_ptr]], #64\n"
-
-      // Apply the back-transformation subtraction.
-      "dup v29.4s, w2\n"  // In parallel, duplicate `clamp_min` into v29.
-      "sub v20.4s, v28.4s, v20.4s\n"
-      "sub v21.4s, v28.4s, v21.4s\n"
-      "sub v22.4s, v28.4s, v22.4s\n"
-      "sub v23.4s, v28.4s, v23.4s\n"
-      "sub v24.4s, v28.4s, v24.4s\n"
-      "sub v25.4s, v28.4s, v25.4s\n"
-      "sub v26.4s, v28.4s, v26.4s\n"
-      "sub v27.4s, v28.4s, v27.4s\n"
 
       // Apply the `clamp_min` bound.
       "dup v30.4s, w3\n"  // In parallel, duplicate `clamp_max` into v30.
@@ -980,8 +957,6 @@ void BinaryKernelNeonOutOfOrder8x4(const BinaryKernelParams<8, 4>& params) {
         "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24", "v25",
         "v26", "v27", "v28", "v29", "v30", "v31");
 }
-
-#undef RUY_OFFSET_BACKTRANSFORM_ADD
 #undef RUY_OFFSET_POST_ACTIVATION_MULTIPLIER
 #undef RUY_OFFSET_POST_ACTIVATION_BIAS
 #undef RUY_OFFSET_LHS_BASE_PTR

larq_compute_engine/tflite/tests/bconv2d_test.cc
@@ -404,7 +404,7 @@ void test_lce_op_output(const std::vector<std::int8_t>& lce_output_data,
     unrounded_builtin_output[i] = unrounded_int8_output;
   }
 
-  EXPECT_THAT(lce_output_data, ::testing::Pointwise(::testing::FloatNear(0.501),
+  EXPECT_THAT(lce_output_data, ::testing::Pointwise(::testing::FloatNear(0.55),
                                                     unrounded_builtin_output));
 }
 

larq_compute_engine/core/bconv2d_impl_ref.h
@@ -35,17 +35,15 @@ using ce::core::TBitpacked;
 
 template <typename AccumScalar, typename DstScalar,
           ce::core::OutputTransformDetails details>
-inline void BConv2D(const ConvParams& params,
-                    const RuntimeShape& packed_input_shape,
-                    const TBitpacked* packed_input_data,
-                    const RuntimeShape& packed_filter_shape,
-                    const TBitpacked* packed_filter_data,
-                    const ce::core::OutputTransform<std::int32_t, DstScalar,
-                                                    details>& output_transform,
-                    const RuntimeShape& output_shape, DstScalar* output_data,
-                    const RuntimeShape& im2col_shape, TBitpacked* im2col_data,
-                    void* padding_buffer, const int pad_value,
-                    void* cpu_backend_context) {
+inline void BConv2D(
+    const ConvParams& params, const RuntimeShape& packed_input_shape,
+    const TBitpacked* packed_input_data,
+    const RuntimeShape& packed_filter_shape,
+    const TBitpacked* packed_filter_data,
+    const ce::core::OutputTransform<DstScalar, details>& output_transform,
+    const RuntimeShape& output_shape, DstScalar* output_data,
+    const RuntimeShape& im2col_shape, TBitpacked* im2col_data,
+    void* padding_buffer, const int pad_value, void* cpu_backend_context) {
   static_assert(std::is_same<DstScalar, float>::value ||
                     std::is_same<DstScalar, TBitpacked>::value ||
                     std::is_same<DstScalar, std::int8_t>::value,

larq_compute_engine/core/bconv2d_output_transform.h
@@ -21,90 +21,125 @@ enum class OutputTransformDetails {
   PreprocessedIntegerOnly
 };
 
-// The `OutputTransform` struct describes what needs to be done to get from the
-// int32 accumulator value to the final result that is written back to memory.
-//
-// This transform depends on the destination type `DstScalar` which can be:
-// - float
-// - TBitpacked (i.e. int32_t, meaning bitpacked output)
-// - int8
-template <typename AccumScalar, typename DstScalar,
+/*
+ * The `OutputTransform` struct describes what needs to be done to get from the
+ * int32 accumulator value to the final result that is written back to memory.
+ */
+template <typename DstScalar,
           OutputTransformDetails = OutputTransformDetails::Default>
 struct OutputTransform {};
 
-// A part of the transformation is common to all output types.
-// This part is described by `OutputTransformBase`.
-template <typename AccumScalar>
-struct OutputTransformBase {
-  AccumScalar backtransform_add = 0;
-  std::int32_t clamp_min = std::numeric_limits<AccumScalar>::lowest();
-  std::int32_t clamp_max = std::numeric_limits<AccumScalar>::max();
+/*
+ * ------------
+ * Float output
+ * ------------
+ *
+ * Conceptually, the float output transform is as follows:
+ *   0. We start with an XOR-popcount accumulator value `accum`. It lies in the
+ *      range {0, ..., K}, where K := `kernel_height` * `kernel_width` *
+ *      `channels_in` is the maximum number of bits that can be set.
+ *   1. Perform a 'back-transformation', which is a linear transformation that
+ *      maps {0, ..., K} onto {-K, ..., K}:
+ *                         K - 2 * accum = -2 * accum + K
+ *      This yields the 'true output' of the binary convolution operation, in
+ *      the sense that it's the same value that you would get by running a
+ *      normal floating-point convolution with +1/-1 input and +1/-1 weights.
+ *   2. Perform a clamp operation that represents a fused activation function.
+ *   3. Convert to float and perform a linear transformation with a multiplier
+ *      and bias that represents a fused batch normalisation layer.
+ *
+ * Thus, the float output transform is:
+ *                 float_cast(clamp(-2 * accum + K)) * mul + bias
+ *
+ * However, we can perform an equivalent, faster computation, by adjusting the
+ * clamp values and fusing (part of) the back-transformation into the
+ * multiplier/bias:
+ *                 float_cast(clamp'(accum << 1)) * mul' + bias'
+ *
+ * Note that the left shift cannot be fused, because the left-shifted
+ * accumulator value will either be always odd or always even, and we support
+ * clamping to arbitrary integers.
+ *
+ * The parameters of the output transform are therefore two int32 clamp values
+ * and float pointers to a multiplier and a bias.
+ */
+template <>
+struct OutputTransform<float, OutputTransformDetails::Default> {
+  std::int32_t clamp_min = std::numeric_limits<std::int32_t>::lowest();
+  std::int32_t clamp_max = std::numeric_limits<std::int32_t>::max();
+  const float* multiplier = nullptr;
+  const float* bias = nullptr;
 
-  inline AccumScalar RunBase(const AccumScalar accum) const {
-    // Backtransform can still be done in int32
-    AccumScalar x = backtransform_add - 2 * accum;
-    // Activation function can also be done in int32
-    x = std::min<AccumScalar>(x, clamp_max);
-    x = std::max<AccumScalar>(x, clamp_min);
-    return x;
+  float Run(const std::int32_t accum, int out_channel) const {
+    TFLITE_DCHECK(multiplier != nullptr);
+    TFLITE_DCHECK(bias != nullptr);
+    std::int32_t x = accum << 1;
+    x = std::max<std::int32_t>(std::min<std::int32_t>(x, clamp_max), clamp_min);
+    return static_cast<float>(x) * multiplier[out_channel] + bias[out_channel];
   }
 };
 
-// Output transformation for float kernels
-template <typename AccumScalar>
-struct OutputTransform<AccumScalar, float, OutputTransformDetails::Default>
-    : OutputTransformBase<AccumScalar> {
-  const float* post_activation_multiplier = nullptr;
-  const float* post_activation_bias = nullptr;
+/*
+ * -----------
+ * Int8 output
+ * -----------
+ *
+ * For int8 output, there are two additional conceptual steps:
+ *   4. Multiply by the reciprocal of the int8 output scale, and add the int8
+ *      zero-point.
+ *   5. Round to an integer and clamp to the int8 range.
+ *
+ * We can fuse step (4) into the existing multiplier/bias, and so the int8
+ * output transform is:
+ *           int8_cast(float_cast(clamp'(accum << 1)) * mul + bias)
+ *
+ * Thus, the int8 output transform parameters are the same as in the float case.
+ */
+template <>
+struct OutputTransform<std::int8_t, OutputTransformDetails::Default> {
+  std::int32_t clamp_min = std::numeric_limits<std::int32_t>::lowest();
+  std::int32_t clamp_max = std::numeric_limits<std::int32_t>::max();
+  const float* multiplier = nullptr;
+  const float* bias = nullptr;
 
-  float Run(const AccumScalar accum, int out_channel) const {
-    TF_LITE_ASSERT(post_activation_multiplier != nullptr);
-    TF_LITE_ASSERT(post_activation_bias != nullptr);
-    // Post multiply and add are done in float
-    float x = static_cast<float>(this->RunBase(accum));
-    x *= post_activation_multiplier[out_channel];
-    x += post_activation_bias[out_channel];
-    return x;
+  std::int8_t Run(const std::int32_t accum, int out_channel) const {
+    TFLITE_DCHECK(multiplier != nullptr);
+    TFLITE_DCHECK(bias != nullptr);
+    // Clamping is done in int32
+    std::int32_t x = accum << 1;
+    x = std::max<std::int32_t>(std::min<std::int32_t>(x, clamp_max), clamp_min);
+    // The linear transformation is done in float
+    float y =
+        static_cast<float>(x) * multiplier[out_channel] + bias[out_channel];
+    // And then we round back to int32 and clamp to the int8 range
+    std::int32_t z = tflite::TfLiteRound(y);
+    z = std::min<std::int32_t>(z, std::numeric_limits<std::int8_t>::max());
+    z = std::max<std::int32_t>(z, std::numeric_limits<std::int8_t>::lowest());
+    return static_cast<std::int8_t>(z);
   }
 };
 
-// Output transformation for bitpacked output
-template <typename AccumScalar>
-struct OutputTransform<AccumScalar, TBitpacked,
-                       OutputTransformDetails::Default> {
-  const AccumScalar* thresholds = nullptr;
+/*
+ * ----------------
+ * Bitpacked output
+ * ----------------
+ *
+ * For writing bitpacked output, the output transform needs to yield a single
+ * bit - the sign of the result of conceptual step (3). In fact, it's possible
+ * to avoid having to do the clamping and the linear transformation first, and
+ * instead compare the accumulator directly against a pre-computed threshold to
+ * work out which bit to return.
+ *
+ * Thus, the bitpacked output transform parameters are a single pointer to an
+ * array of pre-computed thresholds.
+ */
+template <>
+struct OutputTransform<TBitpacked, OutputTransformDetails::Default> {
+  const std::int32_t* thresholds = nullptr;
 
-  bool Run(const AccumScalar accum, int out_channel) const {
-    TF_LITE_ASSERT(thresholds != nullptr);
+  bool Run(const std::int32_t accum, int out_channel) const {
+    TFLITE_DCHECK(thresholds != nullptr);
     return accum > thresholds[out_channel];
-  }
-};
-
-// Output transformation for 8-bit quantization
-template <typename AccumScalar>
-struct OutputTransform<AccumScalar, std::int8_t,
-                       OutputTransformDetails::Default>
-    : OutputTransformBase<AccumScalar> {
-  // These effective values are the post-activation multipliers and biases
-  // divided by output_scale and including the output zero_point
-  const float* effective_post_activation_multiplier = nullptr;
-  const float* effective_post_activation_bias = nullptr;
-
-  std::int8_t Run(const AccumScalar accum, int out_channel) const {
-    TF_LITE_ASSERT(effective_post_activation_multiplier != nullptr);
-    TF_LITE_ASSERT(effective_post_activation_bias != nullptr);
-    // First convert to full precision to do the linear transformation
-    float result_fp = static_cast<float>(this->RunBase(accum));
-    result_fp *= effective_post_activation_multiplier[out_channel];
-    result_fp += effective_post_activation_bias[out_channel];
-    // Now round back to int32
-    AccumScalar result = tflite::TfLiteRound(result_fp);
-    // Clamp to int8 range
-    result =
-        std::min<AccumScalar>(result, std::numeric_limits<std::int8_t>::max());
-    result = std::max<AccumScalar>(result,
-                                   std::numeric_limits<std::int8_t>::lowest());
-    return static_cast<std::int8_t>(result);
   }
 };
 

larq_compute_engine/core/bgemm_impl.h
@@ -34,7 +34,7 @@ void BGemm(const MatrixParams<TBitpacked>& lhs_params,
            const MatrixParams<TBitpacked>& rhs_params,
            const TBitpacked* rhs_data,
            const MatrixParams<DstScalar>& dst_params, DstScalar* dst_data,
-           const OutputTransform<AccumScalar, DstScalar>& params,
+           const OutputTransform<DstScalar>& params,
            CpuBackendContext* context) {
   ruy::profiler::ScopeLabel label("BGemm");
   // TODO: special fast bgemm impl. for matrix-vector multiplication

larq_compute_engine/core/bgemm_impl_ruy.h
@@ -23,12 +23,14 @@ using compute_engine::core::TBitpacked;
 
 template <typename AccumScalar, typename DstScalar>
 struct BGemmImplUsingRuy {
-  static void Run(
-      const MatrixParams<TBitpacked>& lhs_params, const TBitpacked* lhs_data,
-      const MatrixParams<TBitpacked>& rhs_params, const TBitpacked* rhs_data,
-      const MatrixParams<DstScalar>& dst_params, DstScalar* dst_data,
-      const OutputTransform<AccumScalar, DstScalar>& output_transform,
-      CpuBackendContext* context) {
+  static void Run(const MatrixParams<TBitpacked>& lhs_params,
+                  const TBitpacked* lhs_data,
+                  const MatrixParams<TBitpacked>& rhs_params,
+                  const TBitpacked* rhs_data,
+                  const MatrixParams<DstScalar>& dst_params,
+                  DstScalar* dst_data,
+                  const OutputTransform<DstScalar>& output_transform,
+                  CpuBackendContext* context) {
     ruy::profiler::ScopeLabel label("BGemmRuy");
 
     static_assert(std::is_signed<DstScalar>::value,

larq_compute_engine/core/bgemm_kernels_arm32.h
@@ -52,7 +52,6 @@ using compute_engine::core::TBitpacked;
 #define RUY_OFFSET_DEPTH 56
 #define RUY_OFFSET_CLAMP_MIN 60
 #define RUY_OFFSET_CLAMP_MAX 64
-#define RUY_OFFSET_BACKTRANSFORM_ADD 68
 
 #define RUY_STACK_OFFSET_SIZE 96
 #define RUY_STACK_OFFSET_DST_COL_PTR 0
@@ -83,8 +82,6 @@ void CheckOffsetsInKernelParams(const Params&) {
   static_assert(offsetof(Params, depth) == RUY_OFFSET_DEPTH, "");
   static_assert(offsetof(Params, clamp_min) == RUY_OFFSET_CLAMP_MIN, "");
   static_assert(offsetof(Params, clamp_max) == RUY_OFFSET_CLAMP_MAX, "");
-  static_assert(
-      offsetof(Params, backtransform_add) == RUY_OFFSET_BACKTRANSFORM_ADD, "");
 }
 
 // This is a very naive and first attempt on using the SIMD registers for BGEMM.
@@ -269,10 +266,6 @@ void BinaryKernelNeonOutOfOrder4x4(BinaryKernelParams<4, 4>& params) {
       "ldr r5, [sp, #" RUY_STR(RUY_STACK_OFFSET_RHS_COL_PTR) "]\n"
       "mov %[rhs_ptr], r5\n"
 
-      // Load backtransform add (duplicate 4 times into v13)
-      "ldr r1, [%[params], #" RUY_STR(RUY_OFFSET_BACKTRANSFORM_ADD) "]\n"
-      "vdup q13.32, r1 \n"
-
       // post-accumulation transformation:
       //
       // q13 = |BKADD|BKADD|BKADD|BKADD|
@@ -304,20 +297,15 @@ void BinaryKernelNeonOutOfOrder4x4(BinaryKernelParams<4, 4>& params) {
       "vld1.32 {d8, d9, d10, d11}, [%[rhs_ptr]]!\n"
       "vld1.32 {d12, d13, d14, d15}, [%[rhs_ptr]]!\n"
 
-      // Perform the backtransformation (in int32)
+      // Load the clamp_max bound (in parallel with the shift)
+      "ldr r1, [%[params], #" RUY_STR(RUY_OFFSET_CLAMP_MIN) "]\n"
+      "vdup q12.32, r1 \n"  // clamp_min
+
+      // Perform the backtransformation shift (in int32)
       "vshl q8.s32, q8.s32, #1\n"
       "vshl q9.s32, q9.s32, #1\n"
       "vshl q10.s32, q10.s32, #1\n"
       "vshl q11.s32, q11.s32, #1\n"
-
-      // Load the clamp_max bound (in parallel with the sub)
-      "ldr r1, [%[params], #" RUY_STR(RUY_OFFSET_CLAMP_MIN) "]\n"
-      "vdup q12.32, r1 \n"  // clamp_min
-
-      "vsub q8.s32, q13.s32, q8.s32\n"
-      "vsub q9.s32, q13.s32, q9.s32\n"
-      "vsub q10.s32, q13.s32, q10.s32\n"
-      "vsub q11.s32, q13.s32, q11.s32\n"
 
       // Load the clamp_max bound (in parallel with the clamp_min)
       "ldr r2, [%[params], #" RUY_STR(RUY_OFFSET_CLAMP_MAX) "]\n"
@@ -534,7 +522,6 @@ void BinaryKernelNeonOutOfOrder4x4(BinaryKernelParams<4, 4>& params) {
 #undef RUY_OFFSET_DEPTH
 #undef RUY_OFFSET_CLAMP_MIN
 #undef RUY_OFFSET_CLAMP_MAX
-#undef RUY_OFFSET_BACKTRANSFORM_ADD
 
 #endif  // RUY_PLATFORM_NEON && RUY_OPT(ASM) && RUY_PLATFORM_NEON_32
 #endif  // COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_KERNELS_ARM32_H_

larq_compute_engine/core/bgemm_kernels_common.h
@@ -18,7 +18,7 @@ struct BinaryMulParams {
   using AccumScalar = tAccumScalar;
   using DstScalar = tDstScalar;
 
-  OutputTransform<AccumScalar, DstScalar> output_transform;
+  OutputTransform<DstScalar> output_transform;
 
   static constexpr LoopStructure kLoopStructure = LoopStructure::kAuto;
   static constexpr LayoutSupport kLayoutSupport = LayoutSupport::kGeneral;
@@ -36,7 +36,7 @@ struct BinaryMulParams<tAccumScalar, TBitpacked> {
   using AccumScalar = tAccumScalar;
   using DstScalar = TBitpacked;
 
-  OutputTransform<AccumScalar, DstScalar> output_transform;
+  OutputTransform<DstScalar> output_transform;
 
   static constexpr LoopStructure kLoopStructure = LoopStructure::kAuto;
   static constexpr LayoutSupport kLayoutSupport = LayoutSupport::kGeneral;
@@ -69,7 +69,6 @@ struct BinaryKernelParams {
   std::int32_t depth;
   std::int32_t clamp_min;
   std::int32_t clamp_max;
-  std::int32_t backtransform_add;
   float dst_tmp_buf[LhsCols * RhsCols];
 };
 
@@ -90,10 +89,8 @@ inline void MakeBinaryKernelParams(
   params->dst_base_ptr =
       dst->data.get() + start_col * dst->layout.stride + start_row;
 
-  params->post_activation_multiplier =
-      spec.output_transform.post_activation_multiplier;
-  params->post_activation_bias = spec.output_transform.post_activation_bias;
-  params->backtransform_add = spec.output_transform.backtransform_add;
+  params->post_activation_multiplier = spec.output_transform.multiplier;
+  params->post_activation_bias = spec.output_transform.bias;
   params->start_row = start_row;
   params->start_col = start_col;
   params->last_row = end_row - LhsCols;

commit 5420b8f8f356942ef0a2d7e9ec1a42d277f06cc4

Fix bug in 32-bit accumulator kernel introduced by 346f72e.
Also, fix the 'overflow' tests that are meant to test the 32-bit
accumulator kernels but were incorrectly not being run on Aarch64.

larq_compute_engine/core/bgemm_kernels_arm64.h
@@ -310,20 +310,15 @@ void BinaryKernelNeonOutOfOrder4x4(const BinaryKernelParams<4, 4>& params) {
       "ld1 {v0.4s, v1.4s, v2.4s, v3.4s}, [%[lhs_ptr]], #64\n"
       "ld1 {v4.4s, v5.4s, v6.4s, v7.4s}, [%[rhs_ptr]], #64\n"
 
-      // Perform the backtransformation (in int32)
+      // Load the clamp_max bound (in parallel with the shift)
+      "ldr w2, [%[params], #" RUY_STR(RUY_OFFSET_CLAMP_MIN) "]\n"
+      "dup v12.4s, w2\n"  // clamp_min
+
+      // Perform the backtransformation shift (in int32)
       "shl v24.4s, v24.4s, #1\n"
       "shl v25.4s, v25.4s, #1\n"
       "shl v26.4s, v26.4s, #1\n"
       "shl v27.4s, v27.4s, #1\n"
-
-      // Load the clamp_max bound (in parallel with the sub)
-      "ldr w2, [%[params], #" RUY_STR(RUY_OFFSET_CLAMP_MIN) "]\n"
-      "dup v12.4s, w2\n"  // clamp_min
-
-      "sub v24.4s, v13.4s, v24.4s\n"
-      "sub v25.4s, v13.4s, v25.4s\n"
-      "sub v26.4s, v13.4s, v26.4s\n"
-      "sub v27.4s, v13.4s, v27.4s\n"
 
       // Load the clamp_max bound (in parallel with the clamp_min)
       "ldr w3, [%[params], #" RUY_STR(RUY_OFFSET_CLAMP_MAX) "]\n"

larq_compute_engine/tflite/tests/bconv2d_test.cc
@@ -362,7 +362,8 @@ using ::testing::ElementsAreArray;
 void test_lce_op_output(const std::vector<TBitpacked>& lce_output_data,
                         const std::vector<int>& builtin_output_shape,
                         const std::vector<float>& builtin_output_data,
-                        std::int32_t zero_point, float scale) {
+                        std::int32_t zero_point, float scale,
+                        bool high_error_tolerance = false /* ignored */) {
   // Apply bitpacking to the builtin output.
   RuntimeShape out_shape;
   out_shape.BuildFrom(builtin_output_shape);
@@ -383,16 +384,20 @@ void test_lce_op_output(const std::vector<TBitpacked>& lce_output_data,
 void test_lce_op_output(const std::vector<float>& lce_output_data,
                         const std::vector<int>& builtin_output_shape,
                         const std::vector<float>& builtin_output_data,
-                        std::int32_t zero_point, float scale) {
-  EXPECT_THAT(lce_output_data, ::testing::Pointwise(::testing::FloatNear(1e-3),
-                                                    builtin_output_data));
+                        std::int32_t zero_point, float scale,
+                        bool high_error_tolerance = false) {
+  const float tolerance = high_error_tolerance ? 1e-2 : 1e-3;
+  EXPECT_THAT(lce_output_data,
+              ::testing::Pointwise(::testing::FloatNear(tolerance),
+                                   builtin_output_data));
 }
 
 // Output test for int8 output (compared to builtin float output)
 void test_lce_op_output(const std::vector<std::int8_t>& lce_output_data,
                         const std::vector<int>& builtin_output_shape,
                         const std::vector<float>& builtin_output_data,
-                        std::int32_t zero_point, float scale) {
+                        std::int32_t zero_point, float scale,
+                        bool high_error_tolerance = false /* ignored */) {
   // Convert builtin float output to int8, but leave it unrounded.
   // This way, we can spot rounding errors in our int8 implementation.
   std::vector<float> unrounded_builtin_output(builtin_output_data.size());
@@ -653,9 +658,11 @@ void runTest(const TestParam& param) {
 
   // Invoke the op and test that the output is correct.
   m_lce.Invoke();
+  const bool use_high_error_tolerance =
+      filter_height * filter_width * input_depth > 1 << 12;
   test_lce_op_output(m_lce.GetOutput(), m_builtin.GetOutputShape(),
                      builtin_output, output_tensor.zero_point,
-                     output_tensor.scale);
+                     output_tensor.scale, use_high_error_tolerance);
 }
 
 TEST_P(BConv2DOpTest, BitpackedOutput) {
@@ -688,15 +695,15 @@ INSTANTIATE_TEST_SUITE_P(
         ValuesIn(BConv2DOpTest::GetKernelsTuples(*kKernelMap))),
     TestParam::TestNameSuffix);
 
-#if RUY_PLATFORM_ARM_64
-// Separately, for 64-bit optimised kernels only, test a very large input
-// channel and filter size combination that would overflow 16-bit accumulators
-// (to check that we successfully fall back to the 32-bit accumulator kernels).
+// Separately, for optimised kernels only, test a very large input channel and
+// filter size combination that would overflow 16-bit accumulators (to check
+// that we successfully fall back to the 32-bit accumulator kernels if
+// necessary).
 INSTANTIATE_TEST_SUITE_P(
     OptimizedKernel16BitOverflowTest, BConv2DOpTest,
     ::testing::Combine(
-        Values(std::array<int, 4>{1, 8, 8, 2048}),  // input shape [BHWI]
-        Values(std::array<int, 3>{7, 7, 4}),        // filter shape [HWO]
+        Values(std::array<int, 4>{1, 6, 6, 3072}),  // input shape [BHWI]
+        Values(std::array<int, 3>{5, 5, 4}),        // filter shape [HWO]
         Values(std::array<int, 2>{1, 1}),           // strides height/width
         Values(std::array<int, 2>{1, 1}),           // dilation height/width
         Values(Padding_VALID, Padding_ONE),         // padding
@@ -706,7 +713,6 @@ INSTANTIATE_TEST_SUITE_P(
         Values(std::pair<std::string, register_function>{
             "BConv2DOPT", compute_engine::tflite::Register_BCONV_2D_OPT})),
     TestParam::TestNameSuffix);
-#endif
 
 // The BigTest suite will be skipped in the qemu CI runs as they take more than
 // an hour.