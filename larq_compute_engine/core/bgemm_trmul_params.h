larq/compute-engine

commit 7a069fb2ef1e620346de6436885060f4289d64f2

Simplify path selection and remove unnecessary kernel registrations. (#497)
* Simplify path selection and remove unnecessary kernel registrations.

Strip out overly-complicated Ruy path-search implementation and
instead select the path with a simple constexpr based on AccumScalar
and DstScalar. Remove blank kernel registrations that are no longer
necessary.

* Add static asserts for DstScalar in optimised kernel registrations.

larq_compute_engine/core/bgemm_trmul_params.h
@@ -8,32 +8,25 @@
 #include "ruy/path.h"
 #include "ruy/trmul_params.h"
 
-using namespace ruy;
-
 namespace compute_engine {
 namespace tflite {
 
-// This file is entirely copied from TF lite RUY codebase. The only difference
-// is that we populate the TrMulParams with our bgemm kernel.
-
 template <ruy::Path ThePath, typename DstScalar, typename MulParamsType>
-void PopulateBinaryTrMulParams(ruy::TrMulParams* params) {
-  // The optimized code paths don't handle the full generality of Ruy's API.
-  // Fall back to Path::kStandardCpp if necessary.
-  bool fallback_to_standard_cpp = false;
-  if (ThePath != ruy::Path::kStandardCpp) {
-    // The optimized code paths currently only handle the case of all matrices
-    // being column major.
-    if (!ruy::IsColMajorTrMul(*params)) {
-      fallback_to_standard_cpp = true;
-    }
-  }
+void PopulateBgemmTrMulParams(const Mat<TBitpacked>& lhs,
+                              const Mat<TBitpacked>& rhs, Mat<DstScalar>& dst,
+                              const MulParamsType& mul_params,
+                              ruy::TrMulParams* params) {
+  params->src[Side::kLhs] = EraseType(lhs);
+  params->src[Side::kRhs] = EraseType(rhs);
+  params->dst = EraseType(dst);
+  params->mul_params = ToVoidPtr(&mul_params);
 
-  if (fallback_to_standard_cpp) {
-    PopulateBinaryTrMulParams<ruy::Path::kStandardCpp, DstScalar,
-                              MulParamsType>(params);
+  // Optimised code paths only support all matrices being column-major
+  if (ruy::IsColMajorTrMul(*params) && ThePath != ruy::Path::kStandardCpp) {
+    PopulateBgemmTrMulParams<ruy::Path::kStandardCpp>(lhs, rhs, dst, mul_params,
+                                                      params);
     return;
-  }
+  };
 
   using Kernel = BgemmKernel<ThePath, DstScalar, MulParamsType>;
   using LhsKernelLayout = typename Kernel::LhsLayout;
@@ -50,125 +43,6 @@ void PopulateBinaryTrMulParams(ruy::TrMulParams* params) {
   params->run_pack[Side::kRhs] =
       &compute_engine::tflite::RunPack<ThePath, RhsKernelLayout>;
   params->run_kernel = &RunBgemmKernel<ThePath, DstScalar, MulParamsType>;
-}
-
-// PopulateTrMulParamsAllCompiledPaths calls into one of multiple
-// instantiations of PopulateTrMulParams. For each bit that is set in
-// CompiledPaths, it statically instantiates PopulateTrMulParams with a Path
-// corresponding to that single bit. The call to PopulateTrMulParams is
-// guarded by a runtime check that it is in fact the dynamically selected path.
-//
-// PopulateTrMulParamsAllCompiledPaths is implemented with template
-// metaprogramming by mutual recursion between PathSearchCountdown and
-// PathSearchCompiledPaths.
-//
-// PopulateTrMulParamsAllCompiledPaths is logically implementing the following
-// computation:
-//
-// template <Path CompiledPaths>
-// void PopulateTrMulParamsAllCompiledPaths(Path the_path,
-//                                            TrMulParams* params) {
-//   for (int bit = 8 * sizeof(Path) - 1; bit != -1; bit--) { // [1]
-//     Path current_path = static_cast<Path>(1 << bit);
-//     if ((CompiledPaths & current_path) != Path::kNone) { // [2]
-//       if (current_path == the_path) { // [3]
-//         PopulateTrMulParams<current_path, ...>(the_path, params);
-//         return;
-//       }
-//     }
-//   }
-// }
-//
-//
-//
-// [1] - Done by the main definition of PathSearchCountdown. The `bit--` is
-// done in the recursion of PathSearchOnlyCompiledPaths.
-// [2] - Done by PathSearchOnlyCompiledPaths's partial template
-// specialization on InCompiledPaths. This is the check which necessitates
-// doing the whole computation at C++ compile time.
-// [3] - Done by the `if` in the main definition of
-// PathSearchOnlyCompiledPaths.
-//
-// The template metaprogramming is necessary because:
-// - In `PopulateTrMulParams<current_path, ...>`, current_path must be a C++
-// compile-time constant.
-// - PopulateTrMulParamsAllCompiledPaths must not instantiate
-// inner loops for paths that are not in CompiledPaths, since that can result in
-// bogus instantiations which cause a compile time failure.
-template <ruy::Path CompiledPaths, int BitNumber, typename DstScalar,
-          typename MulParamsType>
-struct PathSearchCountdown;
-
-template <ruy::Path CompiledPaths, bool InCompiledPaths, int BitNumber,
-          typename DstScalar, typename MulParamsType>
-struct PathSearchOnlyCompiledPaths {
-  static constexpr ruy::Path kCurrentPath =
-      static_cast<ruy::Path>(1 << BitNumber);
-  static void Search(ruy::Path the_path, ruy::TrMulParams* params) {
-    if (kCurrentPath == the_path) {
-      PopulateBinaryTrMulParams<kCurrentPath, DstScalar, MulParamsType>(params);
-      return;
-    }
-    PathSearchCountdown<CompiledPaths, BitNumber - 1, DstScalar,
-                        MulParamsType>::Search(the_path, params);
-  }
-};
-
-// Skip this iteration if CompiledPaths doesn't contain the specified path.
-template <ruy::Path CompiledPaths, int BitNumber, typename DstScalar,
-          typename MulParamsType>
-struct PathSearchOnlyCompiledPaths<CompiledPaths, false, BitNumber, DstScalar,
-                                   MulParamsType> {
-  static void Search(ruy::Path the_path, ruy::TrMulParams* params) {
-    PathSearchCountdown<CompiledPaths, BitNumber - 1, DstScalar,
-                        MulParamsType>::Search(the_path, params);
-  }
-};
-
-template <ruy::Path CompiledPaths, int BitNumber, typename DstScalar,
-          typename MulParamsType>
-struct PathSearchCountdown {
-  static constexpr ruy::Path kCurrentPath =
-      static_cast<ruy::Path>(1 << BitNumber);
-  static void Search(ruy::Path the_path, ruy::TrMulParams* params) {
-    PathSearchOnlyCompiledPaths<
-        CompiledPaths, (CompiledPaths & kCurrentPath) != ruy::Path::kNone,
-        BitNumber, DstScalar, MulParamsType>::Search(the_path, params);
-  }
-};
-
-// Termination of the countdown. If the counter reaches -1, then we haven't
-// found the specified path.
-template <ruy::Path CompiledPaths, typename DstScalar, typename MulParamsType>
-struct PathSearchCountdown<CompiledPaths, -1, DstScalar, MulParamsType> {
-  static void Search(ruy::Path the_path, ruy::TrMulParams* params) {
-    RUY_DCHECK(false);
-  }
-};
-
-template <ruy::Path CompiledPaths, typename DstScalar, typename MulParamsType>
-void PopulateBinaryTrMulParamsAllCompiledPaths(ruy::Path the_path,
-                                               ruy::TrMulParams* params) {
-  return PathSearchCountdown<CompiledPaths, 8 * sizeof(ruy::Path) - 1,
-                             DstScalar, MulParamsType>::Search(the_path,
-                                                               params);
-}
-
-template <ruy::Path CompiledPaths, typename DstScalar, typename MulParamsType>
-void CreateBinaryTrMulParams(const Mat<TBitpacked>& lhs,
-                             const Mat<TBitpacked>& rhs,
-                             const MulParamsType& mul_params, Ctx* ctx,
-                             Mat<DstScalar>* dst, Path the_path,
-                             TrMulParams* params) {
-  // Fill in the fields we already know.
-  params->src[Side::kLhs] = EraseType(lhs);
-  params->src[Side::kRhs] = EraseType(rhs);
-  params->dst = EraseType(*dst);
-  params->mul_params = ToVoidPtr(&mul_params);
-
-  // Create inner loops and packed matrices based on the Path.
-  PopulateBinaryTrMulParamsAllCompiledPaths<CompiledPaths, DstScalar,
-                                            MulParamsType>(the_path, params);
 }
 
 }  // namespace tflite

larq_compute_engine/core/BUILD
@@ -104,7 +104,6 @@ cc_library(
     hdrs = [
         "bgemm_kernels_arm.h",
         "bgemm_kernels_ruy.h",
-        "bgemm_kernels_x86.h",
     ],
     deps = [
         ":bgemm_kernels_arm",

larq_compute_engine/core/bgemm_impl_ruy.h
@@ -36,7 +36,7 @@ struct BGemmImplUsingRuy {
     static_assert(std::is_signed<DstScalar>::value,
                   "Output of BGEMM should be of a signed type.");
 
-    // getting ruy context
+    // Get ruy context
     auto ruy_ctx = get_ctx(context->ruy_context());
 
     // Set up the matrix layouts and mul_params.
@@ -63,37 +63,27 @@ struct BGemmImplUsingRuy {
     BinaryMulParams<AccumScalar, DstScalar> mul_params;
     mul_params.output_transform = output_transform;
 
-    constexpr auto BGemmCompiledPaths = ruy::kAllPaths;
-
-    ruy::Path bgemm_runtime_path = ruy_ctx->SelectPath(ruy::kAllPaths);
-    RUY_DCHECK_NE(bgemm_runtime_path, ruy::Path::kNone);
-
-#if RUY_PLATFORM_ARM
-    if (bgemm_runtime_path == ruy::Path::kNeonDotprod)
-      bgemm_runtime_path = ruy::Path::kNeon;
-#elif RUY_PLATFORM_X86
-    if (bgemm_runtime_path == ruy::Path::kAvx2 ||
-        bgemm_runtime_path == ruy::Path::kAvx512)
-      bgemm_runtime_path = ruy::Path::kStandardCpp;
+#if RUY_PLATFORM_NEON
+    constexpr bool HasOptimizedNeonKernel =
+        std::is_same<AccumScalar, std::int16_t>::value ||
+        std::is_same<DstScalar, float>::value ||
+        std::is_same<DstScalar, std::int8_t>::value;
+    constexpr auto SelectedPath =
+        HasOptimizedNeonKernel ? ruy::Path::kNeon : ruy::Path::kStandardCpp;
+#else
+    constexpr auto SelectedPath = ruy::Path::kStandardCpp;
 #endif
-
-    // Bitpacked output is only supported in the assembly kernels with int16
-    // accumulators. Otherwise, fall back to the C++ kernel.
-    if (!std::is_same<AccumScalar, std::int16_t>::value &&
-        std::is_same<DstScalar, TBitpacked>::value) {
-      bgemm_runtime_path = ruy::Path::kStandardCpp;
-    }
 
     ruy::Mat<TBitpacked> transposed_lhs(internal_lhs);
     Transpose(&transposed_lhs);
 
-    ruy::TrMulParams binary_trmul_params;
-    CreateBinaryTrMulParams<BGemmCompiledPaths>(
-        transposed_lhs, internal_rhs, mul_params, ruy_ctx, &internal_dst,
-        bgemm_runtime_path, &binary_trmul_params);
+    ruy::TrMulParams bgemm_trmul_params;
+    PopulateBgemmTrMulParams<SelectedPath>(transposed_lhs, internal_rhs,
+                                           internal_dst, mul_params,
+                                           &bgemm_trmul_params);
 
-    HandlePrepackedCaching(&binary_trmul_params, ruy_ctx);
-    ruy::TrMul(&binary_trmul_params, ruy_ctx);
+    HandlePrepackedCaching(&bgemm_trmul_params, ruy_ctx);
+    ruy::TrMul(&bgemm_trmul_params, ruy_ctx);
   }
 };
 

larq_compute_engine/core/bgemm_kernels_arm.h
@@ -19,22 +19,6 @@ using namespace ruy;
 
 using compute_engine::core::TBitpacked;
 
-#if RUY_PLATFORM_NEON
-
-// Generic kNeonDotprod template
-template <typename DstScalar, typename MulParamsType>
-struct BgemmKernel<ruy::Path::kNeonDotprod, DstScalar, MulParamsType> {
-  ruy::Tuning tuning = Tuning::kAuto;
-  using LhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
-  using RhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
-  explicit BgemmKernel(ruy::Tuning tuning_) : tuning(tuning_) {}
-  void Run(const ruy::PMat<TBitpacked>& lhs, const ruy::PMat<TBitpacked>& rhs,
-           const MulParamsType& mul_params, int start_row, int start_col,
-           int end_row, int end_col, ruy::Mat<DstScalar>* dst) const {
-    TFLITE_DCHECK(false);
-  }
-};
-
 #if RUY_PLATFORM_NEON && RUY_OPT(ASM) && RUY_PLATFORM_NEON_32
 // A BGEMM kernel for ARM32 Neon.
 #include "bgemm_kernels_arm32.h"
@@ -51,8 +35,9 @@ struct BgemmKernel<ruy::Path::kNeon, DstScalar,
            const BinaryMulParams<std::int32_t, DstScalar>& mul_params,
            int start_row, int start_col, int end_row, int end_col,
            ruy::Mat<DstScalar>* dst) const {
-    TFLITE_DCHECK((std::is_same<DstScalar, float>::value ||
-                   std::is_same<DstScalar, std::int8_t>::value));
+    static_assert(std::is_same<DstScalar, float>::value ||
+                      std::is_same<DstScalar, std::int8_t>::value,
+                  "");
     BinaryKernelParams<DstScalar, LhsLayout::kCols, RhsLayout::kCols> params;
     MakeBinaryKernelParams(lhs, rhs, start_row, start_col, end_row, end_col,
                            dst, mul_params, &params);
@@ -79,9 +64,10 @@ struct BgemmKernel<ruy::Path::kNeon, DstScalar,
            const BinaryMulParams<std::int16_t, DstScalar>& mul_params,
            int start_row, int start_col, int end_row, int end_col,
            ruy::Mat<DstScalar>* dst) const {
-    TFLITE_DCHECK((std::is_same<DstScalar, float>::value ||
-                   std::is_same<DstScalar, std::int8_t>::value ||
-                   std::is_same<DstScalar, TBitpacked>::value));
+    static_assert(std::is_same<DstScalar, float>::value ||
+                      std::is_same<DstScalar, std::int8_t>::value ||
+                      std::is_same<DstScalar, TBitpacked>::value,
+                  "");
     BinaryKernelParams<DstScalar, LhsLayout::kCols, RhsLayout::kCols> params;
     MakeBinaryKernelParams(lhs, rhs, start_row, start_col, end_row, end_col,
                            dst, mul_params, &params);
@@ -102,8 +88,9 @@ struct BgemmKernel<ruy::Path::kNeon, DstScalar,
            const BinaryMulParams<std::int32_t, DstScalar>& mul_params,
            int start_row, int start_col, int end_row, int end_col,
            ruy::Mat<DstScalar>* dst) const {
-    TFLITE_DCHECK((std::is_same<DstScalar, float>::value ||
-                   std::is_same<DstScalar, std::int8_t>::value));
+    static_assert(std::is_same<DstScalar, float>::value ||
+                      std::is_same<DstScalar, std::int8_t>::value,
+                  "");
     BinaryKernelParams<DstScalar, LhsLayout::kCols, RhsLayout::kCols> params;
     MakeBinaryKernelParams(lhs, rhs, start_row, start_col, end_row, end_col,
                            dst, mul_params, &params);
@@ -112,7 +99,5 @@ struct BgemmKernel<ruy::Path::kNeon, DstScalar,
 };
 
 #endif  // RUY_OPT(ASM) && RUY_PLATFORM_NEON_64
-
-#endif  // RUY_PLATFORM_NEON
 
 #endif  // COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_KERNELS_ARM_H_

larq_compute_engine/core/bgemm_kernels_arm64.h
@@ -903,6 +903,9 @@ IF_BITPACKED_OUTPUT(
       "beq 60f\n"
 
       // Slow path: not all the 8x4 block fits
+      //     Fill v30.b[0] with a bitstring of the form 0...01...1 with `w1`
+      // ones, so that v30 can be used as a mask to keep only the bits in each
+      // bitpacked byte that correspond to actual output values.
       "mov w15, #1\n"
       "lsl w15, w15, w1\n"
       "sub w15, w15, #1\n"

larq_compute_engine/core/bgemm_kernels_ruy.h
@@ -21,8 +21,6 @@ struct BgemmKernel {};
 // TODO: this is hacky
 #if RUY_PLATFORM_NEON
 #include "bgemm_kernels_arm.h"
-#elif RUY_PLATFORM_X86
-#include "bgemm_kernels_x86.h"
 #endif
 
 template <typename DstScalar, typename Spec>

larq_compute_engine/core/bgemm_kernels_x86.h
@@ -1,83 +1,0 @@
-#ifndef COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_KERNELS_X86_H_
-#define COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_KERNELS_X86_H_
-
-#include "larq_compute_engine/core/types.h"
-#include "ruy/common.h"
-#include "ruy/kernel_common.h"
-#include "ruy/mat.h"
-#include "ruy/matrix.h"
-#include "ruy/mul_params.h"
-#include "ruy/opt_set.h"
-#include "ruy/path.h"
-#include "ruy/platform.h"
-#include "ruy/tune.h"
-
-using namespace ruy;
-
-using compute_engine::core::TBitpacked;
-
-#if RUY_PLATFORM_X86
-
-template <typename DstScalar, typename MulParamsType>
-struct BgemmKernel<ruy::Path::kAvx2, DstScalar, MulParamsType> {
-  Tuning tuning = Tuning::kAuto;
-  using LhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
-  using RhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
-  explicit BgemmKernel(Tuning tuning_) : tuning(tuning_) {}
-  void Run(const ruy::PMat<TBitpacked>& lhs, const ruy::PMat<TBitpacked>& rhs,
-           const MulParamsType& mul_params, int start_row, int start_col,
-           int end_row, int end_col, ruy::Mat<DstScalar>* dst) const {
-    static_assert(std::is_signed<DstScalar>::value,
-                  "Output of binary kernel should be of a signed type.");
-    TFLITE_DCHECK(false);
-  }
-};
-
-template <typename DstScalar, typename MulParamsType>
-struct BgemmKernel<ruy::Path::kAvx512, DstScalar, MulParamsType> {
-  Tuning tuning = Tuning::kAuto;
-  using LhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 16>;
-  using RhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 16>;
-  explicit BgemmKernel(Tuning tuning_) : tuning(tuning_) {}
-  void Run(const ruy::PMat<TBitpacked>& lhs, const ruy::PMat<TBitpacked>& rhs,
-           const MulParamsType& mul_params, int start_row, int start_col,
-           int end_row, int end_col, ruy::Mat<DstScalar>* dst) const {
-    static_assert(std::is_signed<DstScalar>::value,
-                  "Output of binary kernel should be of a signed type.");
-    TFLITE_DCHECK(false);
-  }
-};
-
-template <typename DstScalar, typename MulParamsType>
-struct BgemmKernel<ruy::Path::kAvxVnni, DstScalar, MulParamsType> {
-  Tuning tuning = Tuning::kAuto;
-  using LhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 16>;
-  using RhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 16>;
-  explicit BgemmKernel(Tuning tuning_) : tuning(tuning_) {}
-  void Run(const ruy::PMat<TBitpacked>& lhs, const ruy::PMat<TBitpacked>& rhs,
-           const MulParamsType& mul_params, int start_row, int start_col,
-           int end_row, int end_col, ruy::Mat<DstScalar>* dst) const {
-    static_assert(std::is_signed<DstScalar>::value,
-                  "Output of binary kernel should be of a signed type.");
-    TFLITE_DCHECK(false);
-  }
-};
-
-template <typename DstScalar, typename MulParamsType>
-struct BgemmKernel<Path::kSse42, DstScalar, MulParamsType> {
-  Tuning tuning = Tuning::kAuto;
-  using LhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
-  using RhsLayout = FixedKernelLayout<Order::kRowMajor, 1, 8>;
-  explicit BgemmKernel(Tuning tuning_) : tuning(tuning_) {}
-  void Run(const ruy::PMat<TBitpacked>& lhs, const ruy::PMat<TBitpacked>& rhs,
-           const MulParamsType& mul_params, int start_row, int start_col,
-           int end_row, int end_col, ruy::Mat<DstScalar>* dst) const {
-    static_assert(std::is_signed<DstScalar>::value,
-                  "Output of binary kernel should be of a signed type.");
-    TFLITE_DCHECK(false);
-  }
-};
-
-#endif  // RUY_PLATFORM_X86
-
-#endif  // COMPUTE_EGNINE_TFLITE_KERNELS_BGEMM_KERNELS_X86_H_

commit 77037b55d5e4d5921257e25a999402f380f6241f

Fix bug introduced by #497. Make sure OPT kernels run.

larq_compute_engine/core/bgemm_trmul_params.h
@@ -22,7 +22,7 @@ void PopulateBgemmTrMulParams(const Mat<TBitpacked>& lhs,
   params->mul_params = ToVoidPtr(&mul_params);
 
   // Optimised code paths only support all matrices being column-major
-  if (ruy::IsColMajorTrMul(*params) && ThePath != ruy::Path::kStandardCpp) {
+  if (!ruy::IsColMajorTrMul(*params) && ThePath != ruy::Path::kStandardCpp) {
     PopulateBgemmTrMulParams<ruy::Path::kStandardCpp>(lhs, rhs, dst, mul_params,
                                                       params);
     return;