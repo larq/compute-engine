d5174e5e42639811cdd1378865964c80c8b1fc56
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-09-16 16:06:41 +0100

    Add padding to temporary arrays to ensure we don't read beyond bounds.

--- larq_compute_engine/core/types.h
+++ larq_compute_engine/core/types.h
@@ -8,6 +8,11 @@
 namespace compute_engine {
 namespace core {
 
+// In our kernels we may occasionally read (but never write) beyond the end of
+// an array. This is the maximum number of extra bytes that will be read, and
+// should be added as padding to the end of tensor allocations.
+#define LCE_EXTRA_BYTES 16
+
 // Define these once here, so they can be included everywhere.
 using TBitpacked = std::int32_t;
 constexpr std::size_t bitpacking_bitwidth =
