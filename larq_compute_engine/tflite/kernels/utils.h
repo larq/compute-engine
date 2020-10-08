a64156d7fb15843521cebc5209f6d9c8ab7a3b21
Lukas Geiger <lukas.geiger94@gmail.com>
2020-10-09 00:08:38 +0100

    Inline kernel utils functions that convert padding and activation enums

--- larq_compute_engine/tflite/kernels/utils.h
+++ larq_compute_engine/tflite/kernels/utils.h
@@ -7,10 +7,32 @@
 namespace tflite {
 
 // Converts the flatbuffer activation to what is used at runtime.
-TfLiteFusedActivation ConvertActivation(ActivationFunctionType activation);
+inline TfLiteFusedActivation ConvertActivation(
+    ActivationFunctionType activation) {
+  switch (activation) {
+    case ActivationFunctionType_NONE:
+      return kTfLiteActNone;
+    case ActivationFunctionType_RELU:
+      return kTfLiteActRelu;
+    case ActivationFunctionType_RELU_N1_TO_1:
+      return kTfLiteActReluN1To1;
+    case ActivationFunctionType_RELU6:
+      return kTfLiteActRelu6;
+    default:
+      return kTfLiteActNone;
+  }
+}
 
 // Converts the flatbuffer padding enum to what is used at runtime.
-TfLitePadding ConvertPadding(Padding padding);
+inline TfLitePadding ConvertPadding(Padding padding) {
+  switch (padding) {
+    case Padding_SAME:
+      return kTfLitePaddingSame;
+    case Padding_VALID:
+      return kTfLitePaddingValid;
+  }
+  return kTfLitePaddingUnknown;
+}
 
 }  // namespace tflite
 
