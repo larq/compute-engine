e7b3c4fdac081bcd1e66fa28c0956a5c7e4b9fa0
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-10-22 15:09:17 +0100

    Add __version__ property. (#543)

--- larq_compute_engine/__init__.py
+++ larq_compute_engine/__init__.py
@@ -1,4 +1,12 @@
 from larq_compute_engine.mlir.python.converter import convert_keras_model
 from larq_compute_engine.tflite.python import interpreter as testing
 
+try:
+    from importlib import metadata  # type: ignore
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata  # type: ignore
+
+__version__ = metadata.version("larq_compute_engine")
+
 __all__ = ["convert_keras_model", "testing"]
