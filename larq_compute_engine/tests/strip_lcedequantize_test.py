17e6b46fa99571588985757c74b63591896ceb5f
Simon Maurer <simomaur@student.ethz.ch>
2021-04-12 16:53:05 +0200

    added pytest module for verifying lce_dequantize_ops

---
+++ larq_compute_engine/tests/strip_lcedequantize_test.py
@@ -0,0 +1,53 @@
+import sys
+
+import larq as lq
+import numpy as np
+import pytest
+import tensorflow as tf
+
+from larq_compute_engine.mlir.python.converter import convert_keras_model
+from larq_compute_engine.mlir.python.util import strip_lcedequantize_ops
+from larq_compute_engine.tflite.python.interpreter import Interpreter
+
+
+def toy_model_sign(**kwargs):
+    img = tf.keras.layers.Input(shape=(224, 224, 3))
+    x = larq.layers.QuantConv2D(256, kernel_size=3, strides=1, padding="same", pad_values=1, input_quantizer="ste_sign", kernel_quantizer="ste_sign", kernel_constraint="weight_clip")(img)
+    x = larq.quantizers.SteSign()(x)
+    return tf.keras.Model(inputs = img, outputs = x)
+
+def quant(x):
+    return tf.quantization.fake_quant_with_min_max_vars(x, -3.0, 3.0)
+
+def toy_model_int8_sign(**kwargs):
+    img = tf.keras.layers.Input(shape=(224, 224, 3))
+    x = quant(img)
+    x = larq.layers.QuantConv2D(256, kernel_size=3, strides=1, padding="same", pad_values=1, input_quantizer="ste_sign", kernel_quantizer="ste_sign", kernel_constraint="weight_clip")(img)
+    x = larq.quantizers.SteSign()(x)
+    x = quant(x)
+    return tf.keras.Model(inputs = img, outputs = x)
+
+
+@pytest.mark.parametrize("model_cls", [toy_model_sign, toy_model_int8_sign])
+@pytest.mark.parametrize("inference_input_type", [tf.int8, tf.float32])
+@pytest.mark.parametrize("inference_output_type", [tf.int8, tf.float32])
+def test_lcedequantize_input_output(model_cls, inference_input_type, inference_output_type):
+    model_lce = convert_keras_model(
+        model_cls(),
+        inference_input_type=inference_input_type,
+        inference_output_type=inference_output_type,
+        experimental_default_int8_range=(-6.0, 6.0) if model_cls == toy_model else None,
+        experimental_enable_bitpacked_activations=True
+    )
+    model_lce = strip_lcedequantize_ops(model_lce)
+    interpreter = tf.lite.Interpreter(model_content=model_lce)
+    input_details = interpreter.get_input_details()
+    assert len(input_details) == 1
+    assert input_details[0]["dtype"] == inference_input_type.as_numpy_dtype
+    output_details = interpreter.get_output_details()
+    assert len(output_details) == 1
+    assert output_details[0]["dtype"] == tf.int32.as_numpy_dtype
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__, "-s"]))
