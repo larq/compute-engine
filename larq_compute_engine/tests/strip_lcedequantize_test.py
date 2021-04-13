dda14d3de82640b913c21b83ec8dd38da0131392
Simon Maurer <simomaur@student.ethz.ch>
2021-04-13 04:23:03 +0200

    fix PyFlakes error due to typo when defining toy_model

--- larq_compute_engine/tests/strip_lcedequantize_test.py
+++ larq_compute_engine/tests/strip_lcedequantize_test.py
@@ -35,7 +35,7 @@ def test_strip_lcedequantize_ops(model_cls, inference_input_type, inference_outp
         model_cls(),
         inference_input_type=inference_input_type,
         inference_output_type=inference_output_type,
-        experimental_default_int8_range=(-6.0, 6.0) if model_cls == toy_model else None,
+        experimental_default_int8_range=(-6.0, 6.0) if model_cls == toy_model_sign else None,
         experimental_enable_bitpacked_activations=True
     )
     model_lce = strip_lcedequantize_ops(model_lce)
