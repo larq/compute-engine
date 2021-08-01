cc4405997f6af0f9f2bcd0ed5c92bf46600cbac2
Simon Maurer <simomaur@student.ethz.ch>
2021-08-01 16:49:18 +0200

    Extended Unit tests for test_strip_lcedequantize_ops() to parametrize experimental_enable_bitpacked_activations

--- larq_compute_engine/tests/strip_lcedequantize_test.py
+++ larq_compute_engine/tests/strip_lcedequantize_test.py
@@ -49,14 +49,15 @@ def toy_model_int8_sign(**kwargs):
 @pytest.mark.parametrize("model_cls", [toy_model_sign, toy_model_int8_sign])
 @pytest.mark.parametrize("inference_input_type", [tf.float32, tf.int8])
 @pytest.mark.parametrize("inference_output_type", [tf.float32, tf.int8])
+@pytest.mark.parametrize("experimental_enable_bitpacked_activations", [True, False])
 def test_strip_lcedequantize_ops(
-    model_cls, inference_input_type, inference_output_type
+    model_cls, inference_input_type, inference_output_type, experimental_enable_bitpacked_activations
 ):
     model_lce = convert_keras_model(
         model_cls(),
         inference_input_type=inference_input_type,
         inference_output_type=inference_output_type,
-        experimental_enable_bitpacked_activations=True,
+        experimental_enable_bitpacked_activations=experimental_enable_bitpacked_activations,
     )
     model_lce = strip_lcedequantize_ops(model_lce)
     interpreter = tf.lite.Interpreter(model_content=model_lce)
