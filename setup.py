664aa670e388dbc431db8a8dd9f5761624c0f41d
Lucien Gaitskell <lucieng@gaitskell.com>
2021-09-08 14:29:32 +0100

    :arrow_up: 0.6.2 (#683)

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ ext_modules = [Extension("_foo", ["stub.cc"])] if platform.startswith("linux") e
 
 setup(
     name="larq-compute-engine",
-    version=get_version_number(default="0.6.1"),
+    version=get_version_number(default="0.6.2"),
     python_requires=">=3.6",
     description="Highly optimized inference engine for binarized neural networks.",
     long_description=readme(),
