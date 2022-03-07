b2759556443374e49309da102feb818d6add2556
Simon Maurer <simomaur@student.ethz.ch>
2022-03-07 15:35:51 +0100

    second parameter fix in LiteInterpreterWrapper

--- larq_compute_engine/tflite/python/interpreter_wrapper_lite.cc
+++ larq_compute_engine/tflite/python/interpreter_wrapper_lite.cc
@@ -57,7 +57,8 @@ LiteInterpreterWrapper::LiteInterpreterWrapper(
 
 PYBIND11_MODULE(interpreter_wrapper_lite, m) {
   pybind11::class_<LiteInterpreterWrapper>(m, "LiteInterpreter")
-      .def(pybind11::init<const pybind11::bytes&, const int, const bool>())
+      .def(pybind11::init<const pybind11::bytes&, const int, const bool,
+           const bool, const bool>())
       .def_property("input_types", &LiteInterpreterWrapper::get_input_types,
                     nullptr)
       .def_property("output_types", &LiteInterpreterWrapper::get_output_types,
