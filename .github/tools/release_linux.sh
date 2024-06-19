58a5af60ca8129482c98fb7eff3ff50c51675ba0
Tom Bannink <tombannink@gmail.com>
2024-06-19 15:57:23 +0200

    Use Github actions temp disk as bazel root

--- .github/tools/release_linux.sh
+++ .github/tools/release_linux.sh
@@ -3,8 +3,13 @@ set -e -x
 
 python configure.py
 
+# Inside the docker container on github actions there is not
+# enough space for the bazel cache, but a larger disk is mounted at /github_disk
+# so we tell bazel to store everything there
+
 # `release_cpu_linux` will activate absolute paths to files that only exist in the tensorflow/build:2.16-pythonXX docker container
-bazel build :build_pip_pkg \
+bazel --output_user_root=/github_disk/bazel_root \
+  build :build_pip_pkg \
   -c opt \
   --config=release_cpu_linux \
   --copt=-fvisibility=hidden \
