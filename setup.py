1c829ded81011bdfd6bc183ee8f8eb9f46e00142
Tom Bannink <tombannink@gmail.com>
2024-06-20 17:17:28 +0200

    :arrow_up: 0.16.0
    ci-skip

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ ext_modules = [Extension("_foo", ["stub.cc"])] if platform.startswith("linux") e
 
 setup(
     name="larq-compute-engine",
-    version=get_version_number(default="0.13.0"),
+    version=get_version_number(default="0.16.0"),
     python_requires=">=3.10",
     description="Highly optimized inference engine for binarized neural networks.",
     long_description=readme(),
